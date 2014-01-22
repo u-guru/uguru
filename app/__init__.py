ef6be5b43f2734fdf1df96802d89e0078d86260a
Samir Makhani <makhani@berkeley.edu>
2014-01-22 12:36:04 -0800

    added requirement

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,7 @@ db = database.db_session
 #Detects production env
 if os.environ.get('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-else:
-    raise
+
 
 
 from app import views, models
