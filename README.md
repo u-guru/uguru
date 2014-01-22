1c520f1458fef8cf2411e8921359b5d7a239fea2
Samir Makhani <makhani@berkeley.edu>
2014-01-22 12:05:19 -0800

    added case for production env for db

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ alembic upgrade head
 =db migrations=
 alembic revision --autogenerate -m "<insert message here>"
 alembic upgrade head
+
+=first time heroku configuration=
+heroku create <subdomain>
+git push heroku master
+heroku addons:add heroku-postgresql:dev
+heroku pg:promote HEROKU_POSTGRESQL_COLOR
--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 import database
 db = database.db_session
+
+#Detects production env
+if os.environ.get('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 
 from app import views, models
