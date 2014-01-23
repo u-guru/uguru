a1c03c4bdf1209b1c5a14db2329ec8989ea5f2e6
Samir Makhani <makhani@berkeley.edu>
2014-01-22 20:36:21 -0800

    trying another way alembic

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,16 @@ import os
 app = Flask(__name__)
 app.config.from_object('config')
 
-import database
-db = database.db_session
+
 
 #Detects production env
 if os.environ.get('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    db = SQLAlchemy(app)
 else:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    import database
+    db = database.db_session
 
 from app import views, models
