a5b6ee00e8853c17e5c878ef27a11da09d8ee1d2
Samir Makhani <makhani@berkeley.edu>
2014-02-13 21:30:29 -0800

    modifications to new page

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
     $(window).resize(function() {
         if ($(window).width() <= 700) {
             $('#how-it-works-main').css('top', '90%');
-            $('.main').css('top','50%');
+            bottom_px = (parseInt($('#how-it-works-main').css("top").replace('px','')) - 250)+ 'px'
+            $('.main').css('top',bottom_px);
             $('#home-button-1').attr('class', 'btn btn-sm')
             $('#home-button-1').css('padding', '10px 15px')
             $('#home-button-2').css('padding', '10px 15px')
             $('#home-button-2').attr('class', 'btn btn-sm')
             $('.logo_main').attr('style', 'font-size:4em;margin-top:-20px')
             $('#welcome_text').css('font-size','1em')
-            $('.welcome').css('margin-top','30px')
+            $('.welcome').css('margin-top','40px')
+            $('#how-it-works-main').css('font-size','1.2em')
         }
         else {
             $('#how-it-works-main').css('top', '85%');
@@ -20,6 +22,7 @@ $(document).ready(function(){
             $('#home-button-2').css('padding', '15px 30px')
             $('.logo_main').attr('style', 'font-size: 7em; margin-top: -35px;')
             $('#welcome_text').css('font-size','1.5em')
+            $('#how-it-works-main').css('font-size','1.7em')
         }
     }).resize(); 
 })
