2b42cab08679a34769bf70242a28ac9b7a7773a2
Samir Makhani <makhani@berkeley.edu>
2014-02-17 18:25:29 -0800

    toggles working on the front

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
     $('#email-on').click(function(){
-        alert("sup");
+        $('#email-on-span').attr('class','btn-toggle-on');
+        $('#email-off-span').attr('class','btn-toggle-off');
     });
     $('#email-off').click(function(){
-        alert("sup");
+        $('#email-on-span').attr('class','btn-toggle-on-inactive');
+        $('#email-off-span').attr('class','btn-toggle-off-active');
     });
     $('#text-off').click(function(){
-        alert("sup");
+        $('#text-on-span').attr('class','btn-toggle-on-inactive');
+        $('#text-off-span').attr('class','btn-toggle-off-active');
     });
     $('#text-on').click(function(){
-        alert("sup");
+        $('#text-on-span').attr('class','btn-toggle-on');
+        $('#text-off-span').attr('class','btn-toggle-off');
     });
 });
