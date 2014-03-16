3033441d2df0f0658b36bb65ecd5f543b0bbf681
Samir Makhani <makhani@berkeley.edu>
2014-03-15 17:53:52 -0700

    new homepage

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
     
-    $(window).resize(function() {
-        if ($(window).width() <= 700) {
-            $('#how-it-works-main').css('top', '87%');
-            bottom_px = (parseInt($('#how-it-works-main').css("top").replace('px','')) - 220)+ 'px'
-            $('.main').css('top',bottom_px);
-            $('#home-button-1').attr('class', 'btn btn-sm')
-            $('#home-button-1').css('padding', '10px 15px')
-            $('#home-button-2').css('padding', '10px 15px')
-            $('#home-button-2').attr('class', 'btn btn-sm')
-            $('.logo_main').attr('style', 'font-size:4em;margin-top:-25px')
-            $('#welcome_text').css('font-size','1em')
-            $('.welcome').css('margin-top','40px')
-            $('#how-it-works-main').css('font-size','1.2em')
-            $('#home-button-group button').css('margin', '5px')
-            $('#login-text').css('font-size', '.9em')
-        }
-        else {
-            $('#how-it-works-main').css('top', '85%');
-            $('.main').css('top','40%');            
-            $('#home-button-1').attr('class', 'btn btn-lg')
-            $('#home-button-2').attr('class', 'btn btn-lg')
-            $('#home-button-1').css('padding', '15px 30px')
-            $('#home-button-2').css('padding', '15px 30px')
-            $('#welcome_text').css('font-size','1.5em')
-            $('.logo_main').attr('style', 'font-size: 7em; margin-top: -35px;')
-            $('#welcome_text').css('font-size','1.5em')
-            $('#how-it-works-main').css('font-size','1.7em')
-            $('#home-button-group button').css('margin', '10px')
-            $('#login-text').css('font-size', '1.3em')
-        }
-    }).resize(); 
+    // $(window).resize(function() {
+    //     if ($(window).width() <= 700) {
+    //         $('#how-it-works-main').css('top', '87%');
+    //         bottom_px = (parseInt($('#how-it-works-main').css("top").replace('px','')) - 220)+ 'px'
+    //         $('.main').css('top',bottom_px);
+    //         $('#home-button-1').attr('class', 'btn btn-sm')
+    //         $('#home-button-1').css('padding', '10px 15px')
+    //         $('#home-button-2').css('padding', '10px 15px')
+    //         $('#home-button-2').attr('class', 'btn btn-sm')
+    //         $('.logo_main').attr('style', 'font-size:4em;margin-top:-25px')
+    //         $('#welcome_text').css('font-size','1em')
+    //         $('.welcome').css('margin-top','40px')
+    //         $('#how-it-works-main').css('font-size','1.2em')
+    //         $('#home-button-group button').css('margin', '5px')
+    //         $('#login-text').css('font-size', '.9em')
+    //     }
+    //     else {
+    //         $('#how-it-works-main').css('top', '85%');
+    //         $('.main').css('top','40%');            
+    //         $('#home-button-1').attr('class', 'btn btn-lg')
+    //         $('#home-button-2').attr('class', 'btn btn-lg')
+    //         $('#home-button-1').css('padding', '15px 30px')
+    //         $('#home-button-2').css('padding', '15px 30px')
+    //         $('#welcome_text').css('font-size','1.5em')
+    //         $('.logo_main').attr('style', 'font-size: 7em; margin-top: -35px;')
+    //         $('#welcome_text').css('font-size','1.5em')
+    //         $('#how-it-works-main').css('font-size','1.7em')
+    //         $('#home-button-group button').css('margin', '10px')
+    //         $('#login-text').css('font-size', '1.3em')
+    //     }
+    // }).resize(); 
 
     $('#register-skills').on('click', '.skill-tag-remove', function(e){
       e.preventDefault();
@@ -84,16 +84,14 @@ $(document).ready(function(){
     })
 
     $('#login-link').click(function() {
-        $('#home-top').hide();
-        $('#how-it-works-main').hide();
+        $('#home').hide();
         $('body').css('background-color','white')
         // $('#login-page').show();
         // $("#login-page").animate({width:'toggle'},350);
         $('#login-page').show('slide', {direction: 'right'}, 200);
     });
     $('#tutor-signup-btn').click(function() {
-        $('#home-top').hide();
-        $('#how-it-works-main').hide();
+        $('#home').hide();
         $('body').css('background-color','white')
         // $('#login-page').show();
         // $("#login-page").animate({width:'toggle'},350);
@@ -101,8 +99,7 @@ $(document).ready(function(){
         // $('#tutor-signup').show('slide', {direction: 'right'}, 200);
     });
     $('#student-signup-btn').click(function() {
-        $('#home-top').hide();
-        $('#how-it-works-main').hide();
+        $('#home').hide();
         $('body').css('background-color','white')
         // $('#login-page').show();
         // $("#login-page").animate({width:'toggle'},350);
