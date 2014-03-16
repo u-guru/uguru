fe79eeb13fc1d6d7ee2384bd177171aefc92af6f
Samir Makhani <makhani@berkeley.edu>
2014-03-16 16:11:58 -0700

    how it works

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,36 +1,17 @@
 $(document).ready(function(){
     
-    // $(window).resize(function() {
-    //     if ($(window).width() <= 700) {
-    //         $('#how-it-works-main').css('top', '87%');
-    //         bottom_px = (parseInt($('#how-it-works-main').css("top").replace('px','')) - 220)+ 'px'
-    //         $('.main').css('top',bottom_px);
-    //         $('#home-button-1').attr('class', 'btn btn-sm')
-    //         $('#home-button-1').css('padding', '10px 15px')
-    //         $('#home-button-2').css('padding', '10px 15px')
-    //         $('#home-button-2').attr('class', 'btn btn-sm')
-    //         $('.logo_main').attr('style', 'font-size:4em;margin-top:-25px')
-    //         $('#welcome_text').css('font-size','1em')
-    //         $('.welcome').css('margin-top','40px')
-    //         $('#how-it-works-main').css('font-size','1.2em')
-    //         $('#home-button-group button').css('margin', '5px')
-    //         $('#login-text').css('font-size', '.9em')
-    //     }
-    //     else {
-    //         $('#how-it-works-main').css('top', '85%');
-    //         $('.main').css('top','40%');            
-    //         $('#home-button-1').attr('class', 'btn btn-lg')
-    //         $('#home-button-2').attr('class', 'btn btn-lg')
-    //         $('#home-button-1').css('padding', '15px 30px')
-    //         $('#home-button-2').css('padding', '15px 30px')
-    //         $('#welcome_text').css('font-size','1.5em')
-    //         $('.logo_main').attr('style', 'font-size: 7em; margin-top: -35px;')
-    //         $('#welcome_text').css('font-size','1.5em')
-    //         $('#how-it-works-main').css('font-size','1.7em')
-    //         $('#home-button-group button').css('margin', '10px')
-    //         $('#login-text').css('font-size', '1.3em')
-    //     }
-    // }).resize(); 
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 600);
+          return false;
+        }
+      }
+    });
 
     $('#register-skills').on('click', '.skill-tag-remove', function(e){
       e.preventDefault();
