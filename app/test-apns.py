67175c277d347157eec4ea90c5d74cb58d333e66
Samir Makhani <makhani@berkeley.edu>
2014-07-23 18:29:06 -0700

    lakjsdklasjda

--- app/test-apns.py
+++ app/test-apns.py
@@ -3,19 +3,16 @@ from apns import APNs, Frame, Payload
 
 apns = APNs(use_sandbox=True, cert_file='uguru-cert.pem', key_file='uguru-key.pem')
 
-token_hex = 'b15c34d2639300184ed4b29cba73ed4f897bcfb7092b0cf6d090edf389e56256'
+token_hex = '91258cbd9d521d53ca5d5a6534a82bb9aef13ee9071b3067af4598a93cc02922'
 payload = Payload(alert="Samir needs help with CS10. You can make $30", sound="default", badge=1)
 apns.gateway_server.send_notification(token_hex, payload)
 
-
-token_hex = 'b15c34d2639300184ed4b29cba73ed4f897bcfb7092b0cf6d090edf389e56256'
 payload = Payload(alert="You've been matched with Samir. Message now", sound="default", badge=1)
 apns.gateway_server.send_notification(token_hex, payload)
 
-token_hex = 'b15c34d2639300184ed4b29cba73ed4f897bcfb7092b0cf6d090edf389e56256'
 payload = Payload(alert="You have received one new message from Michael", sound="default", badge=1)
 apns.gateway_server.send_notification(token_hex, payload)
 
-token_hex = 'b15c34d2639300184ed4b29cba73ed4f897bcfb7092b0cf6d090edf389e56256'
+
 payload = Payload(alert="You just made $45. Cash out now.", sound="default", badge=1)
 apns.gateway_server.send_notification(token_hex, payload)
