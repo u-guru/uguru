bda1b6a287c768ff7ea9204f1ecf53ef4d6e0a76
Samir Makhani <makhani@berkeley.edu>
2014-08-13 08:28:46 -0700

    small bug with returning the skill_name

--- app/api.py
+++ app/api.py
@@ -547,7 +547,7 @@ def api(arg, _id):
 
                 #View Calendar
                 n_detail['request'] = sanitize_dict(r.__dict__)
-                n_detail['request']['skill_name'] = n.feed_message.split(" ")[-1][:-1].replace('<b>', '').replace('</b>', '')
+                n_detail['request']['skill_name'] = n.feed_message.split(" ")[-1].replace('<b>', '').replace('</b>', '')
 
             if n.payment_id:
                 p = Request.query.get(n.payment_id)
