5cdccb9b8d183092af8249d0a68c7cc77abf54d9
Samir Makhani <makhani@berkeley.edu>
2014-11-24 20:00:41 -0800

    CRITICAL fix. Logging not imported in emails.py, this is prevening ALL messages from going through.

--- app/emails.py
+++ app/emails.py
@@ -1,6 +1,7 @@
 import os
 import smtplib
 import mandrill
+import logging
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase # TODO : Imported but unused
