f677b5774026f4646783c8d83f2fa2b6a507f0a8
cameronehrlich <cameronehrlich@gmail.com>
2014-11-26 20:49:37 -0800

    alerted the configuation in preparation from migrations.

--- app/__init__.py
+++ app/__init__.py
@@ -16,14 +16,11 @@ root.addHandler(ch)
 # TODO : Add debug logger
 
 app = Flask(__name__)
-app.config.from_pyfile('../config.py')
+app.config.from_object('config')
 
 #Detects production env
-if os.environ.get('DATABASE_URL'): # TODO : can this be os.environ.get('PRODUCTION') ?
+if os.environ.get('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-else:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class DataBase():
     session = None;
--- config.py
+++ config.py
@@ -4,8 +4,13 @@ SECRET_KEY = '\xc8]{\x9e\xb7\x93\xc1X\xab#\x8b"K\xe8\xd2\xc5\x17C!I\r$\xca\x7f'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# TODO : Move all environment variable shit to here
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# Detects if on Heroku
+if os.environ.get('DATABASE_URL'):
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 S3_LOCATION = os.environ['S3_LOCATION']
 S3_KEY = os.environ['S3_KEY']
