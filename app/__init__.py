f7dfcf9e7591b866c960925b077d836ec411f742
cameronehrlich <cameronehrlich@gmail.com>
2014-12-04 21:56:46 -0800

    changes to database config and .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -30,14 +30,7 @@ assets = Environment(app)
 assets.url = app.static_url_path
 scss = Bundle('css/web.scss', filters='pyscss', output='css/all.css')
 # TODO: Use this line when we have more css files to bundble
-assets.register('scss_all', scss) 
-
-#Detects production env
-if os.environ.get('DATABASE_URL'): # TODO : can this be os.environ.get('PRODUCTION') ?
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-else:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+assets.register('scss_all', scss)
 
 db = SQLAlchemy(app)
 
