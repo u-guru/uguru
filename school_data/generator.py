cf7c0ab0f277e536b89a8a2c1626ce5262c6d95d
cameronehrlich <cameronehrlich@gmail.com>
2014-12-11 02:58:37 -0800

    generator working, 311 schools loaded so far

--- school_data/generator.py
+++ school_data/generator.py
@@ -1,13 +1,13 @@
 import csv
-import json
-# from geopy.geocoders import Nominatim
-from geopy.geocoders import GoogleV3
+from geopy.geocoders import GoogleV3, Nominatim, ArcGIS, OpenMapQuest, Yandex
 from tld import get_tld
+from datetime import datetime
 import pickle
+import time
 
 class School:
 	def __init__(self, row):
-		self.name=row["Institution_Name"]
+		self.name = row["Institution_Name"]
 		self.state = row["Institution_State"] 
 		self.city = row["Institution_City"]
 		self.address = row["Institution_Address"]
@@ -17,9 +17,11 @@ class School:
 		self.domain = get_tld("http://" + self.website, fail_silently=True) # Try to get likely email domain
 		self.latitude = None
 		self.longitude = None
+		self.geocoder_used = None
+		self.last_updated = datetime.now()
 
 	def __repr__(self):
-		return "<School - %s, %s, %s, %s, %s, %s, %s, (%s, %s) >" % (self.name, self.address, self.city, self.state, self.website, self.domain, self.phone, self.latitude, self.longitude)
+		return "<School - %s, %s, %s, %s, %s, %s, %s, %s, %s >" % (self.name, self.address, self.city, self.state, self.website, self.domain, self.phone, self.latitude, self.longitude)
 
 	def __hash__(self):
 		return hash(self.name) ^ hash(self.address)
@@ -38,21 +40,20 @@ class School:
 		return filter(lambda school: address.lower() in school.address.lower(), school_list)
 
 	@staticmethod
-	def nearbySchools(lat_long=(None, None), school_list=[], num_returned=3):
+	def nearby_schools(lat_long=(None, None), school_list=[], num_returned=3):
 		from geopy.distance import vincenty
 
 		discovered_schools = []
 		for school in school_list: # TODO : This can be massively optimized.
 			distance_between = vincenty(lat_long, (school.latitude, school.longitude) ).meters
-			distance_to_school = (distance_between, school)
+			distance_to_school = (distance_between, school) # (123.134, <School>)
 			discovered_schools.append(distance_to_school)
 
 		sorted_schools = sorted(discovered_schools, key=lambda x: x[0])
 		return sorted_schools[:num_returned]
 
 def load_data():
-	# Note : The most recent data can be pulled from this 
-	# government accreditation site: 
+	# Note : The most recent data can be pulled from this government accreditation site: 
 	# http://ope.ed.gov/accreditation/GetDownLoadFile.aspx 
 	csv_file = "Accreditation_2014_10.csv"
 	all_schools = []
@@ -64,57 +65,65 @@ def load_data():
 	return all_schools
 
 def fetch_geo_info(school):
-	geolocator = GoogleV3()
-	try:
-		location = geolocator.geocode(school.address + " " + school.city + " " + school.state)
-		if not location:
-			raise
-			return False
-	except Exception, e:
-		print e
-		return False
-	school.latitude = location.latitude
-	school.longitude = location.longitude
-	return True
+	providers = [GoogleV3, Nominatim, ArcGIS, OpenMapQuest, Yandex]
+	location = None;
+	for geolocator in providers:	
+		try:
+			print "...trying prvider: "+ str(geolocator.__module__) + " for school " + str(school.name)
+			location = geolocator().geocode(school.address + " " + school.city + " " + school.state)
+		except Exception, e:
+			# print "......" + str(e)
+			continue
+		if location:
+			school.latitude = location.latitude
+			school.longitude = location.longitude
+			school.geocoder_used = geolocator.__module__
+			school.last_updated = datetime.now()
+			return school
+	return None
 
-def run(debug=False):
+def run(batch_size=10):
 	# Get all schools and get rid of non-institutions: trade schools, beauty schools, etc...
 	schools = load_data()
 	schools = filter(lambda x: x.type == "Institutional", schools)
 	schools = list(set(schools)) # Remove duplicates 
 	print "Total unique schools to fetch: " + str(len(schools))
 	
-	if debug:
-		print "DEBUG: Shuffling and limiting to 10 schools."
-		import random
-		random.shuffle(schools)
-		schools = schools[:10]
+	# Shuffle them so we dont get the same order every run
+	import random
+	random.shuffle(schools)
+	schools = schools[:batch_size]
 	
-	schools_with_geo = []
-	schools_without_geo = []
-	
-	for school in schools:
-		success = fetch_geo_info(school)
-		if success:
-			print "SUCCESS: " + str(school)
-			schools_with_geo.append(school)
-		else:
-			print "FAIL: " + str(school)
-			schools_without_geo.append(school)
+	school_cache = loadFromFile() # Pull in already computed schools from the cache
 
-	# TODO : Try failed schools again with a different geolocator provider...
-	print "Schools located: " + str(len(schools_with_geo))
-	print "Schools not located: " + str(len(schools_without_geo))
-	saveToFile(schools_with_geo)
-	return schools_with_geo
+	intersection = list(set(schools) & set(school_cache))
+	# print "Cache Hits: "+str(len(intersection))
+	for c_school in intersection:
+		schools.remove(c_school)
+
+	schools_with_geo = []
+	for school in schools:
+		updated_school = fetch_geo_info(school)
+		if not updated_school:
+			print "FAIL: " + str(school)
+		else:
+			print "SUCCESS: " + str(updated_school)
+			schools_with_geo.append(updated_school)
+
+	all_schools = schools_with_geo + school_cache
+	saveToFile(all_schools)
 
 def saveToFile(school_list):
+	print "Schools saved: " + str(len(school_list))
 	pickle.dump(school_list, open("schools.p", "wb"))
-	print "Saved to file."
 
 def loadFromFile():
-	imported_schools = pickle.load(open("schools.p", "rb"))
-	print "Loaded from file."
+	imported_schools = []
+	try:
+		imported_schools = pickle.load(open("schools.p", "rb"))
+		print "Loaded "+str(len(imported_schools))+" schools from local file."
+	except Exception:
+		print "Failed to load schools from local file."
 	return imported_schools
 
 # Alternatives:
