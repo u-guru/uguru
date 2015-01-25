6af418c04b97618b55b33ba9525da56b03d2199f
Samir Makhani <sam1rm@berkeley.edu>
2015-01-25 11:41:12 -0800

    adding extra hardware plugins

--- app/static/remote/js/controllers.js
+++ app/static/remote/js/controllers.js
@@ -14,36 +14,40 @@ angular.module('uguru.student.controllers', [])
   function($scope, $state, $ionicPopup, $timeout, $localstorage, 
   	$cordovaDialogs, Version) {
 
+  	console.log('sup;')
+ //    $scope.showWelcomePopup = function() {
 
-    $scope.showWelcomePopup = function() {
-
-		var popupOptions = {
+	// 	var popupOptions = {
 	     
-	     templateUrl: "templates/welcomePopup.html",
-	     cssClass: "popup-uguru",
-	     scope: $scope
-	   }
+	//      templateUrl: "templates/welcomePopup.html",
+	//      cssClass: "popup-uguru",
+	//      scope: $scope
+	//    }
 
-	   //initialize the popup
-	   $scope.popup = $ionicPopup.show(popupOptions);
+	//    //initialize the popup
+	//    $scope.popup = $ionicPopup.show(popupOptions);
 
-	   	$timeout(function() {
-	   		document.getElementById("popup-cancel-btn").addEventListener("click", function() {
-                console.log('negative clicked');
-                $scope.popup.close();
-		    });
-		    document.getElementById("popup-positive-btn").addEventListener("click", function() {
-		        console.log('positive clicked');
-		        $scope.popup.close();
-		    });
-	   	}, 500);
+	//    	$timeout(function() {
+	//    		document.getElementById("popup-cancel-btn").addEventListener("click", function() {
+ //                console.log('negative clicked');
+ //                $scope.popup.close();
+	// 	    });
+	// 	    document.getElementById("popup-positive-btn").addEventListener("click", function() {
+	// 	        console.log('positive clicked');
+	// 	        $scope.popup.close();
+	// 	    });
+	//    	}, 500);
 
-	};
+	// };
 
-	 //show popup after 1 second
-	 $timeout(function() {
-	 	$scope.showWelcomePopup();
-	 }, 1000);
+	$scope.launchCourseModal = function() {
+		console.log('clicked');
+	}
+
+	 // //show popup after 1 second
+	 // $timeout(function() {
+	 // 	$scope.showWelcomePopup();
+	 // }, 1000);
     
 
   }
