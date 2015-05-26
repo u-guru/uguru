2a28fdbdfb4e7d584336c909d3a944a69e7de1b5
Samir Makhani <sam1rm@berkeley.edu>
2015-05-26 08:08:23 -0700

    modified new admin dashboard

--- app/lib/admin.py
+++ app/lib/admin.py
@@ -3,33 +3,9 @@ admin_info = {
         'name': 'Samir Makhani',
         'profile_url': '/static/img/admin/samir.png'
     },
-    'jasmine@uguru.me': {
-        'name': 'Jasmine Mir',
-        'profile_url': '/static/img/admin/jasmine.png'
-    },
-    'rob@uguru.me': {
-        'name': 'Rob Neivert',
-        'profile_url': '/static/img/admin/rob.png'
-    },
-    'richard@uguru.me': {
-        'name': 'Richard Akhmerov',
-        'profile_url': '/static/img/admin/richard.png'
-    },
     'jon@uguru.me': {
         'name': 'Jon Lewis',
         'profile_url': '/static/img/admin/umi.png'
-    },
-    'geet@uguru.me': {
-        'name': 'Geet P.',
-        'profile_url': '/static/img/admin/geet.png'
-    },
-    'pardo@uguru.me': {
-        'name': 'Pardo Walter',
-        'profile_url': '/static/img/admin/pardo.png'
-    },
-    'varada@uguru.me': {
-        'name': 'Varada Gavasker',
-        'profile_url': '/static/img/admin/varada.png'
     },
     'ben@uguru.me': {
         'name': 'Ben Dalal',
@@ -38,5 +14,13 @@ admin_info = {
     'jason@uguru.me': {
         'name': 'Jason Huang',
         'profile_url': 'https://s3.amazonaws.com/uguruprof/request_file_id_122.png'
+    },
+    'jourdan@uguru.me': {
+        'name': 'Jourdan *',
+        'profile_url': 'https://graph.facebook.com/10152573868267292/picture?width=100&height=100'
+    }
+    'randy@uguru.me': {
+        'name': 'Randy Kim',
+        'profile_url': 'http://i.imgur.com/JwVzEaz.jpg'
     }
 }
