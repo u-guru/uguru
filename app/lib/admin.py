b7627983adecd957c8008417b8d6c3bbeca701b2
Samir Makhani <sam1rm@berkeley.edu>
2015-06-29 09:14:02 -0700

    bug fixed

--- app/lib/admin.py
+++ app/lib/admin.py
@@ -1,19 +1,15 @@
 admin_info = {
     'samir@uguru.me': {
         'name': 'Samir Makhani',
-        'profile_url': '/static/img/admin/samir.png'
-    },
-    'jon@uguru.me': {
-        'name': 'Jon Lewis',
-        'profile_url': '/static/img/admin/umi.png'
+        'profile_url': 'https://drive.google.com/file/d/0By5VIgFdqFHda210bnFYdnhRbVU/view?usp=sharing'
     },
     'ben@uguru.me': {
         'name': 'Ben Dalal',
-        'profile_url': 'https://graph.facebook.com/10152573868267292/picture?width=100&height=100'
+        'profile_url': 'https://drive.google.com/file/d/0By5VIgFdqFHdX1gxSDMyWFlFcjg/view?usp=sharing'
     },
     'jason@uguru.me': {
         'name': 'Jason Huang',
-        'profile_url': 'https://s3.amazonaws.com/uguruprof/request_file_id_122.png'
+        'profile_url': 'https://drive.google.com/file/d/0By5VIgFdqFHdQTRUYnFZdG9rZXc/view?usp=sharing'
     },
     'jourdan@uguru.me': {
         'name': 'Jourdan *',
@@ -21,6 +17,6 @@ admin_info = {
     },
     'randy@uguru.me': {
         'name': 'Randy Kim',
-        'profile_url': 'http://i.imgur.com/JwVzEaz.jpg'
+        'profile_url': 'https://drive.google.com/file/d/0By5VIgFdqFHdNWNHTWhrQWhuYzQ/view?usp=sharing'
     }
 }
