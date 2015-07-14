fcba22d87fb8ac1fd4963883bc16f1a508becf22
Samir Makhani <sam1rm@berkeley.edu>
2015-07-14 11:20:39 -0700

    {test test}

--- mobile-app/scripts/remote_push.sh
+++ mobile-app/scripts/remote_push.sh
@@ -1,8 +1,37 @@
+source ~/.bash_profile
 echo "Clearing iOS xCode cache..."
 rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache"
 echo 'cd to uguru-mobile repository'
 ### TODO: RAISE if um is not defined
-um
+um && echo
+echo '================'
+echo '=============='
+echo '==========='
+echo
+echo 'Please type in a commit message:'
+echo
+echo '>>>'
+echo
+read msg
+echo
+echo 'saving.....'
+echo
+echo 'saving....'
+echo
+echo 'saving...'
+echo
+echo 'saving..'
+echo
+echo 'saving.'
+echo
+echo 'saving..'
+echo
+echo 'saving...'
+echo
+echo 'saving....'
+echo
+echo 'saving.....'
+echo
 echo 'minifying + compressing local assets..'
 gulp -b
 echo 'compiling all local assets..'
@@ -19,20 +48,8 @@ cp mobile-app/www/remote/css/android.css app/static/remote/
 cp mobile-app/www/remote/windows.css app/static/remote/
 echo 'calling git add -A .'
 git add -A .
-echo
-echo 'Please type in a commit message:'
-echo '>>>'
-echo
-read msg
-echo
-echo 'saving.....'
-echo
-echo 'saving....'
-echo
-echo 'saving...'
-echo
-echo 'saving..'
-echo
-echo 'saving.'
 git commit -m "{$msg}"
+echo
+echo
 echo 'commit successful'
+echo
