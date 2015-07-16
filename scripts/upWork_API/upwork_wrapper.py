5a68a4d80869918b271f9c43421ecd10f7e7d740
Ben Dahal <Ben@samirs-mbp.gateway.sonic.net>
2015-07-16 16:00:18 -0700

    Re-updated the script

--- scripts/upWork_API/upwork_wrapper.py
+++ scripts/upWork_API/upwork_wrapper.py
@@ -1,6 +1,6 @@
 import upwork, httplib2, requests, urlparse, dryscrape
 from urlparse import urlparse, parse_qs
-import sys, pprint
+import sys, pprint, json
 
 class DevNull:
 	def write(self,msg):
@@ -16,7 +16,7 @@ secret = '918aeab388eeb19d'
 client = upwork.Client(key,secret)
 authorize_url =  client.auth.get_authorize_url()
 print authorize_url
-requests.get(authorize_url)
+check_in = requests.get(authorize_url)
 verifier = raw_input('Enter oauth_verifier: ')
 oauth_access_token, oauth_access_token_secret = client.auth.get_access_token(verifier)
 client = upwork.Client(key, secret,
@@ -24,18 +24,46 @@ client = upwork.Client(key, secret,
  	                   oauth_access_token_secret=oauth_access_token_secret)
 
 #~016e686fe0d68ba380 --> ACTUAL SHIT
-#jobs_list = client.hr.get_jobs('1850815')
-#print jobs_list
+#jobs_list = client.hr.get_jobs('1850815','1','open')
+#pp = pprint.PrettyPrinter(indent = 4)
+#pp.pprint(jobs_list)
+
+#list_of_companies = client.hr.get_companies()
+#pp = pprint.PrettyPrinter(indent = 4)
+#pp.pprint(list_of_companies)
 #category = 'Web Development'
 #sub_category = 'Web Programming'
 #client.hr.update_job('~016e686fe0d68ba380','1850815','Heres my new titles','this be my new ssdescription','private',category,sub_category, duration = 100, status = 'open')
-job_key = '~016e686fe0d68ba380'
+#job_key = '~016e686fe0d68ba380'
 
 #skills_list = client.provider.get_skills_metadata()
 #pp = pprint.PrettyPrinter(indent = 4)
 #pp.pprint(skills_list)
 #get_job_prof = client.job.get_job_profile(job_key)
 #print get_job_prof
+#freelancer_id = '7719882'
+#profile_details = client.provider.get_provider('~011d8724f956a759b0')
+#pp = pprint.PrettyPrinter(indent = 4)
+#pp.pprint(profile_details)
+
+
+owner = '1850815'
+job_key = '~014754850f959aacfa'
+
+
+with open ('job_applications.json') as f:
+	get_me_data = json.load(f)
+	json_string = json.dumps(get_me_data, sort_keys = True, indent = 2)
+#	print json_string
+	parent = get_me_data['applications']
+	for item in parent:
+
+		get_me_the_best_worker = client.provider.get_provider_brief(item['contractor_ciphertext'])
+		#print get_me_the_best_worker
+		parent_data =  get_me_the_best_worker['dev_adj_score']
+		for datas in parent_data:
+			if datas <= '5':
+				
 def get_list_of_categories():
 		sys.stderr = DevNull()
 		meta_data = client.provider.get_categories_metadata()
@@ -96,17 +124,25 @@ def get_job_profile(job_key):
 		print "Job profile successfully loaded!"
 		return upwork_wrapper.get_job_profile(client.job.get_job_profile)
 
-def hire_someone(job_key,cover_message,owner):
+def hire_someone(job_key,cover_message,freelancer_id):
 		sys.stderr = DevNull()
-		client.hr_v1.invite_to_interview(job_key,cover_message,owner)
+		client.hr_v1.invite_to_interview(job_key,cover_message,freelancer_id)
 		print "Successfully hired!"
 		return upwork_wrapper.hire_someone(client.hr_v1.invite_to_interview)
+
+def get_client_offer(owner):
+		list_client_offers = client.offers.get_client_offer(owner)
+		pp = pprint.PrettyPrinter(indent = 4)
+		pp.pprint(list_client_offers)
+		print "Client Offers successfully loaded"
+		return upwork_wrapper.get_client_offer(client.offers.get_client_offer)
 
 def list_client_offers(owner):
 		sys.stderr = DevNull()
 		client.offers.list_client_offers(owner)
 		print "Client offers loaded!"
 		return upwork_wrapper.list_client_offers(client.offers.list_client_offers)
+
 
 def send_client_offer(title,hourly,budget,message_to_contractor,owner,contractor_name,context,milestones):
 		sys.stderr = DevNull()
@@ -115,10 +151,20 @@ def send_client_offer(title,hourly,budget,message_to_contractor,owner,contractor
 		return upwork_wrapper.send_client_offer(client.offers.send_client_offer)
 
 def list_job_applications(owner,job_key):
-		sys.stderr = DevNull()
-		client.hr_v3.list_client_applications(owner,job_key)
-		print "Job applications successfully loaded!"
+	#	sys.stderr = DevNull()
+		free_lancer_offers = client.hr_v3.list_client_applications(owner,job_key)
+		with open('job_applications.json', 'w') as fp:
+			json.dump(free_lancer_offers,fp,indent = 4)
+		print "Job applications successfully loaded in a json file"
 		return upwork_wrapper.list_job_applications(client.hr_v3.list_client_applications)
+
+
+def get_best_job_application(owner,job_key):
+		sys.stderr = DevNull()
+		#get_me_best_free_lancer = client.hr_v3.list_client_applications(owner,job_key)['contractor_ciphertext']
+
+
+
 
 def create_a_mile_stone(owner,milestones_descrption,deposit_amount,due_date):
 		#sys.stderr = DevNull()
@@ -168,4 +214,30 @@ def reject_a_milestone_submission(submission_id,note_to_freelancer):
 		client.hr_v3.reject_submission(submission_id,note_to_freelancer)
 		print "Successfully rejected the offer!"
 		return upwork_wrapper.reject_a_milestone_submission(client.hr_v3.reject_submission)
+
+def get_active_messages(owner):
+		#sys.stderr = DevNull()
+		active_messages = client.mc.get_trays(owner)
+		pp = pprint.PrettyPrinter(indent = 4)
+		pp.pprint(get_active_messages)
+		return upwork_wrapper.get_active_messages(owner)
+
+def get_message_content(owner,inbox):
+		#sys.stderr = DevNull
+		message_content = client.mc.get_tray_content(owner,inbox)
+		pp = pprint.PrettyPrinter(indent = 4)
+		pp.pprint(get_active_messages)
+		return upwork_wrapper.get_message_content(owner,inbox)
+
+def get_thread(owner,thread_id):
+		sys.stderr = DevNull
+		message_content = client.mc.get_thread_content(owner,thread_id)
+		pp = pprint.PrettyPrinter(indent = 4)
+		pp.pprint(get_active_messages)
+		return upwork_wrapper.get_message_content(owner,thread_id)
+
+def send_message(owner,recipients,body,bcc):
+		sys.stderr = DevNull
+		client.mc.post_message(owner,recipients,body,bcc)
+		return upwork_wrapper.send_message(owner,recipients,body,bcc)
 
