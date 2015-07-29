e68b31a3d6fce170f870e447b9ef8b656355c735
Samir Makhani <sam1rm@berkeley.edu>
2015-07-28 19:38:08 -0700

    {another attempt}

--- mobile-app/www/remote/js/main.js
+++ mobile-app/www/remote/js/main.js
@@ -21,11 +21,11 @@ if (LOCAL) {
   img_base = '/static/'
 }
 
-if (WINDOWS) {
-  var BASE_URL = 'http://uguru-rest.herokuapp.com/production/app/';
-  var REST_URL = 'http://uguru-rest.herokuapp.com';
-  BASE = 'remote/';
-}
+// if (WINDOWS) {
+//   var BASE_URL = 'http://uguru-rest.herokuapp.com/production/app/';
+//   var REST_URL = 'http://uguru-rest.herokuapp.com';
+//   BASE = 'remote/';
+// }
 
 // windows special case
 // if (WINDOWS || navigator.userAgent.match(/iemobile/i) || navigator.userAgent.match(/Windows Phone/i)  || navigator.userAgent.match(/IEMobile/i) || navigator.userAgent === 'Win32NT') {
