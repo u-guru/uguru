a7d19e545ac1d4afd0e67acf4a681b7274bdacef
Gabrielle Wee <mail@gabriellew.ee>
2015-08-20 23:10:13 -0700

    hooked chat button to chat popup

--- app/static/gabrielle/js/main.js
+++ app/static/gabrielle/js/main.js
@@ -139,7 +139,7 @@ $(function () {
 			e.preventDefault();
 		});
 
-		$('#link-support').on("click", function(e) {
+		$('.top-link-chat').on("click", function(e) {
 			document.querySelector('.intercom-launcher-button').click();
 		});
 
