a51694152edef5ecc8be4798b5ae2697682e0297
samir makhani <sam1rm@berkeley.edu>
2015-08-22 09:18:57 -0700

    staging samirs scripts

--- wikipedia.py
+++ wikipedia.py
@@ -3,7 +3,7 @@ def generate_university_url(school_name):
     "&rvprop=timestamp%7Cuser%7Ccomment%7Ccontent&inprop=url&redirects&format=json&iiprop=url&rvparse"
     return url_format
 
-def scrape_all_universities(university_names):
+def scrape_all_universities(university_names, iteration_num):
     university_results = []
     index = 0
     errors = 0
@@ -22,12 +22,12 @@ def scrape_all_universities(university_names):
                 errors += 1
             print index, '/' , length, 'processed.', errors, 'errors.'
 
-            save_results(university_results, 'wikipedia_university_data_2.json')
+            save_results(university_results, 'wikipedia_university_data_%s.json' % iteration_num)
 
         except:
-            save_results(university_results, 'wikipedia_university_data_2.json')
+            save_results(university_results, 'wikipedia_university_data_%s.json' % iteration_num)            
             error_arr.append(university_name)
-            save_results(error_arr, 'wikipedia_errors_2.json')
+            save_results(error_arr, 'wikipedia_errors_%s.json' %iteration_num)
             print "error for", university_name
             errors += 1
         index += 1
@@ -260,6 +260,167 @@ def count_unprocessed():
     save_results(unprocessed_universities, 'wiki_unprocessed.json')
     return
 
+def uni_obj_to_arr(uni):
+    result_dict = {
+        'id':uni.id, 
+        'city':uni.city, 
+        'state':uni.state, 
+        'name': uni.name.replace('Of', 'of').replace('At', 'at').replace(' & ', 'and').replace('The', 'the').replace('And', 'and').replace('-', ' ').replace('/', ' ').replace("'",""),
+        'latitude': uni.latitude,
+        'longitude': uni.longitude,
+        'rank': uni.us_news_ranking,
+        # 'popular_courses': [],
+        'logo_url': uni.logo_url,
+        'population': uni.population,
+        'school_color_one': uni.school_color_one,
+        'school_color_two': uni.school_color_two,
+        'banner_url':uni.banner_url
+    }
+    if uni.fa15_start:
+        result_dict['fa15_start'] = True
+    return result_dict
+
+def update_outdated_universities():
+    from app.lib.all_schools_updated import school_dict
+    ranked_unis = []
+    ranked_unis = json.load(open('wiki_og_rank.json'))
+
+    count = 0
+    for uni in ranked_unis:
+        uni_id = uni['id']
+
+        u = University.query.get(int(uni_id))
+        uni_info = school_dict.get(str(u.id))
+        if not uni_info:
+            continue
+        if uni_info.get('popular_courses'):
+            popular_courses = []
+            for popular_course in uni_info['popular_courses']:
+                if len(popular_course) >= 5 and len(popular_course) <= 9 and not all(char.isdigit() for char in popular_course):
+                    popular_courses.append(popular_course)
+            if popular_courses:
+                uni['popular_courses'] = popular_courses
+
+    with open('wiki_og_rank.json', 'wr') as fp:
+        json.dump(ranked_unis, fp, indent = 4)
+
+    
+    print count
+
+def update_flicker():
+    from app.lib.flickr_wrapper import *
+    uni_arr = json.load(open('wiki_og_rank.json'))
+    count = 0
+    for uni_json in uni_arr:
+        u = University.query.get(int(uni_json['id']))
+        flickr_response = str(search_university_response_api(u))
+        photos_arr = parse_flickr_response(flickr_response)
+        processed_arr = process_returned_photos(photos_arr)
+        processed_arr = sorted(processed_arr, key=lambda k:k['views'], reverse=True)[:20]
+        if processed_arr:
+            count += 1 
+            u.banner_url = str(processed_arr[0]['url'])
+            print u.id, u.name, u.banner_url
+    from app.database import *
+    db_session.commit()
+    print count
+
+
+def recent_to_update_rank():
+    import manage
+    wiki_recent_arr = json.load(open('wikipedia_most_recent2.json'))
+    count = 0
+    count2 = 0
+    for uni in wiki_recent_arr:
+        if not uni.get('id'):
+            continue
+        u = University.query.get(int(uni['id']))
+        if u and u.us_news_ranking > 0:
+            
+            all_images = uni['images']['all']
+            logo_images = uni['images']['logo']
+            seal_images = uni['images']['seal']
+            hex_color = uni['colors']
+            if (all_images or logo_images or seal_images) and hex_color:
+                if hex_color:
+                    if len(hex_color) == 2:
+                        u.school_color_one = hex_color[0].lower()
+                        u.school_color_two = hex_color[1].lower()
+                        # print u.id, u.name, u.school_color_one
+                        count += 1
+                    if len(hex_color) == 1:
+                        u.school_color_one = hex_color[0].lower()
+                        # print u.id, u.name, u.school_color_one
+                        count += 1
+            # if uni.get('population') and uni.get('population').get('students'):
+            #     uni.population = int(uni.get('population').get('students'))
+
+                # count += 1
+                # # print u.us_news_ranking, len(images),'images', len(hex_color), 'colors'
+
+    from app.database import db_session    
+    db_session.commit()
+    print count
+
+def get_total_popular_courses():
+    import json
+    uni_arr = json.load(open('wiki_og_rank.json'))
+    return len([uni.get('popular_courses') for uni in uni_arr if uni.get('popular_courses')])
+
+def get_total_targetted():
+    import json
+    uni_arr = json.load(open('wiki_og_rank.json'))
+    targetted = []
+    count = 0
+    for uni_obj in uni_arr:
+        keys = uni_obj.keys()
+        keys_with_non_nil_values = [key for key in keys if uni_obj[key]]
+        if len(keys) == len(keys_with_non_nil_values):
+            count += 1
+            targetted.append(uni_obj)
+
+    with open('wiki_og_rank_targetted.json', 'wr') as fp:
+        json.dump(targetted, fp, indent = 4)
+    return count
+
+
+
+def calc_stats_uni_arr(uni_arr):
+    total = len(uni_arr)
+    total_cities = len([uni.get('city') for uni in uni_arr if uni.get('city')])
+    total_states = len([uni.get('states') for uni in uni_arr if uni.get('state')])
+    total_ranks = len([uni.get('rank') for uni in uni_arr if uni.get('rank')])
+    total_courses = get_total_popular_courses()
+    total_logos = len([uni.get('popular_courses') for uni in uni_arr if uni.get('logo_url')])
+    total_school_color_ones = len([uni.get('school_color_one') for uni in uni_arr if uni.get('school_color_one')])
+    total_populations = len([uni.get('populations') for uni in uni_arr if uni.get('population')])
+    start_dates = len([uni.get('fa15_start') for uni in uni_arr if uni.get('fa15_start')])
+    banner_urls = len([uni.get('banner_url') for uni in uni_arr if uni.get('banner_url')])
+    print 'total:', total 
+    print 'num cities', "%d/%d" % (total_cities, total)
+    print 'num state', "%d/%d" % (total_states, total)
+    print 'num ranks', "%d/%d" % (total_ranks, total)
+    print 'num populations', "100%"
+    print
+    print 'num logos', "%d/%d" % (total_logos, total), '-', '%d' % (int((total_logos / (total * 1.0)) *100)) + '%'
+    print 'num colors', "%d/%d" % (total_school_color_ones, total), '-', '%d' % (int((total_school_color_ones / (total * 1.0)) *100)) + '%'
+    print 'num start_dates', "%d/%d" % (start_dates, total), '-', '%d' % (int((start_dates / (total * 1.0)) *100)) + '%'
+    print 'num flickr', "%d/%d" % (banner_urls, total), '-', '%d' % (int((banner_urls / (total * 1.0)) *100)) + '%'
+    print 'num popular courses', "%d/%d" % (total_courses, total), '-', '%d' % int((total_courses / (total * 1.0)) *100) + '%'
+    print 
+    print 'num_targetted', "%d/%d" % (total_targetted, total), '-', '%d' % int((total_targetted / (total * 1.0)) *100) + '%'
+    
+
+def generate_universities_dump():
+    from app.lib.all_schools_updated import school_dict
+    formatted_arr = []
+    to_process_later = []
+    uni_arr = University.query.all()
+    from pprint import pprint
+    uni_arr = [uni_obj_to_arr(uni) for uni in University.query.all() if uni.us_news_ranking]
+    calc_stats_uni_arr(uni_arr)
+    with open('wiki_og_rank.json', 'wr') as fp:
+        json.dump(uni_arr, fp, indent = 4)
 
 if __name__ == '__main__':
     import sys, json
@@ -268,21 +429,45 @@ if __name__ == '__main__':
 
     # start = int(args[1])
     # end = int(args[2])
-    # # Iterates through chegg
-    # index = start
-    # SCHOOLS = sorted(University.query.all(), key=lambda k:k.id)
-    # SCHOOLS = [school.name for school in SCHOOLS]
-    # scrape_all_universities(SCHOOLS)
-    uni_arr = json.load(open('wiki_unprocessed.json'))
+    # count = 0    
 
+    #for all schools that have 
+
+    ## 1. popular courses [-]
+    ## 2. name [x]
+    ## 3. city [x] 
+    ## 4. state [x]
+    ## 8. us news rating [x]
+    ## 5. hex color [-]
+    ## 6. some logo [-]
+    ## 7. fa start [-]
+    ## 7. flickr [-]
+
+    ## afterwards --update admin
+    ## 1. targetted universities
+    ## 2. most popular
+    ## 3. us ranked
+    ## 4. manage.py 
+    ## 5. link staging db with uguru-rest
+    ## 6. create new universities.json script that works with the home page
+    ## 7. home page
+
+    # import json
+    # arr = json.load(open('wiki_og.json'))
+    # uni_names = [uni['name'] for uni in arr][start:end]
+
+    # scrape_all_universities(uni_names, 7)
+    # us_news_ranking_only()
+    # generate_universities_dump()
+    # recent_to_update_rank()
+    # generate_universities_dump()
+    get_total_targetted()
+    # update_outdated_universities()
+    # recent_to_update_rank()
     # reparse all the names
-    for uni in uni_arr:
-        uni['name'] = uni['name'].replace('Of', 'of').replace('At', 'at').replace(' & ', 'and').replace('The', 'the').replace('And', 'and').replace('-', ' ').replace('/', ' ').replace("'","")
-    uni_names = [uni['name'] for uni in uni_arr]
-    from pprint import pprint
-    scrape_all_universities(uni_names)
+    # for uni in uni_arr:
+    #     uni['name'] = uni['name'].replace('Of', 'of').replace('At', 'at').replace(' & ', 'and').replace('The', 'the').replace('And', 'and').replace('-', ' ').replace('/', ' ').replace("'","")
+    # uni_names = [uni['name'] for uni in uni_arr]
+    # from pprint import pprint
+    
 
-
-
-    # with open('wikipedia_university_data_2.json', 'wr') as fp:
-    #     json.dump(arr, fp, indent = 4)
