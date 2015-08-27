1fb65187d5bb61a14fc6262e3ece497b94b962ed
nicknaky <nicknaky@users.noreply.github.com>
2015-08-27 13:03:23 -0700

    angular starter kit

---
+++ app/ang-uguru/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angUguruApp
+ * @description
+ * # angUguruApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angUguruApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
