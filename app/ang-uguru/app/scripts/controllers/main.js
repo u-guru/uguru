502bdf52d575b41f451b8622b7b2e5cfd74b32c0
nicknaky <nicknaky@users.noreply.github.com>
2015-08-27 14:07:16 -0700

    angular starter

---
+++ app/ang-uguru/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angUguruApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angUguruApp
+ */
+angular.module('angUguruApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
