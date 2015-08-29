7474a3625e8e4d42b901c3d527ceebc961d78470
Samir Makhani <sam1rm@berkeley.edu>
2015-08-29 15:15:34 -0700

    gpa implementation & integration from gabrielles codebase

--- mobile-app/www/remote/js/controllers/dev/gpa.ctrl.js
+++ mobile-app/www/remote/js/controllers/dev/gpa.ctrl.js
@@ -19,6 +19,45 @@ angular.module('uguru.util.controllers')
     $ionicSideMenuDelegate,$ionicActionSheet)
   {
 
+    $scope.gpaPage = {gpa:0};
+    $scope.tempCourse = {courseGrade:'C', courseCredit:4, courseName:''};
+    $scope.userCourses = [];
+
+    $scope.validateAndSaveCourse = function(courseDict) {
+      console.log(courseDict);
+      //case one --> user did not select a course || grade || did not finis
+      if (!validateCourseInput(courseDict)) {
+        alert('please fill out all fields!');
+        return;
+      }
+
+      $scope.userCourses.push(courseDict);
+      $localstorage.setObject('userGPACourseList', $scope.userCourses);
+      $scope.gpaPage.gpa = calculateGpa($scope.userCourses);
+
+      $scope.success.show(0, 1000, 'Saved!');
+      $scope.addCourseGPAModal.hide();
+
+    }
+
+    var calculateGpa = function(arrCourses) {
+      sum = 0;
+      grades = ['F', 'D', 'C', 'B', 'A'];
+      for (i = 0; i < arrCourses.length; i ++) {
+        course = arrCourses[i];
+        courseGrade = course.courseGrade;
+        numberGrade = grades.indexOf(courseGrade);
+        sum += numberGrade;
+      }
+
+      return (sum / (arrCourses.length * 1.0)).toPrecision(2);
+
+    }
+
+    var validateCourseInput = function(courseDict) {
+      return (courseDict.courseName && courseDict.courseCredit && courseDict.courseGrade);
+    }
+
     //action
     $scope.showAttachActionSheet = function() {
        var options = [{text: 'A'},
@@ -39,22 +78,42 @@ angular.module('uguru.util.controllers')
         }
     });
     }
-    
 
-    $scope.launchCourse = function(verb_index) {
-        console.log("Course Session");
-        $scope.root.vars.last_verb_index_clicked = 3;
-          // if (verb_index) {
-          //   $scope.root.vars.detailed_verbs_index_clicked = 1;
-          // }
 
+    $scope.removeCourseList = function(tempCourse) {
+      for (i = 0; i < $scope.userCourses.length; i++) {
+        course = $scope.userCourses[i];
+        if (course.courseName === tempCourse.courseName) {
+          $scope.userCourses.splice(i, i+1);
+          break;
+        }
+      }
+      $scope.success.show(0, 2000, 'Course ' + tempCourse.courseName + ' removed!');
+      $localstorage.setObject('userGPACourseList', $scope.userCourses);
+      $scope.tempCourse = {courseGrade:'C', courseCredit:4, courseName:''};
+      $scope.addCourseGPAModal.hide();
+    }
+
+
+    $scope.launchAddCourseGPAModal = function(course) {
+      if (course) {
+        $scope.tempCourse = course;
+        $scope.tempCourse.removeButton = true;
+      }
           $ionicModal.fromTemplateUrl(BASE + 'templates/dev/gpa.modal.html', {
               scope: $scope,
               animation: 'slide-in-up'
           }).then(function(modal) {
-            $scope.requestModal = modal;
-            $scope.requestModal.show();
+            $scope.addCourseGPAModal = modal;
+            $scope.addCourseGPAModal.show();
           });
     }
+
+    $scope.$on('$ionicView.enter', function() {
+      $timeout(function() {
+        $scope.userCourses = $localstorage.getObject('userGPACourseList');
+        $scope.gpaPage.gpa = calculateGpa($scope.userCourses);
+      }, 500)
+    });
   }
 ]);
