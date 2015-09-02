953059730f75c5ae3c8bf4a8ccd0054e92e3a049
TwFoB Work <twfobwork@ubuntu.ubuntu-domain>
2015-09-01 20:56:04 -0700

    #4,8-skip,#12,14 still have tiny problem

---
+++ app/test/Release_One_Web/whypane.js
@@ -0,0 +1,6 @@
+describe("#8 Carousel slide 4 up/down arrow doesn't always animate to indicate first/last screenshot.", function () {
+	    // var UguruHomepage = new AngularHomepage();
+	    // var SchoolInput = dv.findElement(by.id('search-bar'));
+
+	
+	});
