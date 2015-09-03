911ed1ad50f0d0b94862424b88ffe9383386ca22
TwFoB Work <twfobwork@ubuntu.ubuntu-domain>
2015-09-03 12:52:29 -0700

    delete ng-uguru folder

--- app/ang-uguru/app/scripts/old/faq.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-$(function () {
-    $("#faq dl dt a").each(function(e){
-        $(this).on("click", function(e) {
-            $(this).parent().parent().toggleClass("active");
-            e.preventDefault();
-        });
-    });
-});
