91d1a25d9c016c6bb3a11f76c6c9f08738852a2e
Samir Makhani <sam1rm@berkeley.edu>
2015-09-05 22:37:16 -0700

    cleaned up university.ctrl.js

---
+++ app/test/Release_One_Web/faq.js
@@ -0,0 +1,40 @@
+
+describe('FAQ Test', function () {
+    // var UguruHomepage = new AngularHomepage();
+    // var SchoolInput = dv.findElement(by.id('search-bar'));
+	describe("#28 FAQ question bars don't always expand.", function () {
+
+		it("Loading Homepage",function()
+		{
+			isAngularSite(false);
+			web.get('http://uguru.me/faq');
+			browser.sleep(8000);
+		});
+		// it("Click Banner",function()
+		// {
+		// 	// dv.wait(EC.visibilityOf(element(by.id(".pure-u-1"))),10000);
+
+		// 	//web.getElement(by.tagName("dl"))
+		// 	//console.log(dv.findElement(by.tagName("dl")));
+		// 	// web.getElement(by.linkText('What kind of question is this?')).click();
+		// 	browser.sleep(10000);
+
+		// })
+	});
+	describe("#16 Side menu doesn't close when browser is scrolled down.", function () {
+		it("Open Side Menu & Go Work Pane",function()
+		{
+			// dv.wait(EC.visibilityOf(element(by.id(".pure-u-1"))),10000);
+			web.getElement(by.css('.top-link-menu.btn-round.btn-ghost')).click();
+		});
+		it("scroll down",function()
+		{
+			dv.wait(EC.visibilityOf(element(by.id("link-work"))),1000);
+			browser.executeScript('window.scrollTo(0,100);');
+		});
+		it("Check Side Menu",function()
+		{
+	 		expect(web.getElement(by.css('.top-link-menu.btn-round.btn-ghost')).isDisplayed()).toBe(false);
+		});
+	});
+});
