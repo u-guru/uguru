c03d42d2a03945178ed152499d013fb8c9fe2ee9
Jason <f230536@github.com>
2015-09-16 19:01:39 -0700

    Acces updated

--- mobile-app/e2e_test/test_case/mobileTest/access.js
+++ mobile-app/e2e_test/test_case/mobileTest/access.js
@@ -6,7 +6,32 @@ describe('Access Page Test', function () {
 	var load= element(by.id('E2E-spinner'))
 	var back = element(by.css('[ng-click="goToAccess()"]'))
 	var listOfCase =  doc.generateRandomString(["","1"],5,"cool")
-   
+   	
+   	describe("Is Page Dragalbe",function()
+   	{
+   		it("drag left",function()
+   		{
+				element(by.id('access')).getLocation().then(function(result)
+   				{
+		   			expect(result.x).toBe(0,"location X is moved");
+
+   				});
+   			doc.slideView(0,"left")
+   		});
+   		it('Check element',function()
+   		{
+   			element(by.id('access')).getLocation().then(function(result)
+   				{
+		   			expect(result.x).toBe(0);
+   				});
+   			browser.sleep(10000);
+   		});
+   		it('Go Bakc Access',function()
+   		{
+   			browser.refresh();
+   		})
+
+   	});
 	
 	for( i = 0; i < listOfCase.length; ++ i)
 	{
@@ -24,7 +49,7 @@ describe('Access Page Test', function () {
 				});
 	 
 
-           		it("Check Fail or Successed",function()
+           		it("Check Message is shown Correct",function()
 				{
 					startButton.click();
 					if(testSpec == 'cool')
@@ -67,7 +92,7 @@ describe('Access Page Test', function () {
 			});
 		});
 
-		it("Check Successed",function()
+		it("Check Message is shown :Access Granted",function()
 		{
 			startButton.click();
 			doc.checkMsg("Access Granted");
