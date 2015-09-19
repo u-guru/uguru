2970304dbc64b709d02d86263c470dc36a02a62c
Ben Dahal <Ben@Samirs-MacBook-Pro.local>
2015-09-18 17:58:14 -0700

    Added yelp script

--- scripts/Yelp/yelp.py
+++ scripts/Yelp/yelp.py
@@ -1,2 +1,15 @@
-from yelp import ReviewSearchApi
-results = ReviewSearchApi(client_key=<my_key>, output="json").by_location("Mission, San Francisco CA")
+from yelpapi import YelpAPI
+import json
+consumer_key = 'J6G3zEBCox3lUfKCcJBLDA'
+consumer_secret = 'oXcvu0rV8NFvSMoRgf_FGZf-72Y'
+token = 'So3cdsVQ-4zB0AAD8MF9VnuDF7XFsihw'
+token_secret = 'PAi0q1eI-lFMI9Az0ZWQ_qEFQ_k'
+latitude = 37.870
+longitude = 122.259
+yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
+search_results = yelp_api.search_query(cll=[latitude,longitude])
+with open('yelp_business.json','wb') as outfile:
+	json.dump(search_results,outfile,indent=4)
+print search_results
+# business_results = yelp_api.business_query(id=business_id, other_args)
+# phone_search_results = yelp_api.phone_search_query(phone=phone_number, other_args)
