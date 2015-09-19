9dc113a2196b6c1ae3d9450620fbd39c6f456c05
Ben Dahal <Ben@Samirs-MacBook-Pro.local>
2015-09-19 15:57:31 -0700

    unified json file

--- scripts/Yelp/yelp.py
+++ scripts/Yelp/yelp.py
@@ -30,21 +30,33 @@ def get_school_data_information(city_info):
 		city_array.append(dictionary)
 		with open(output,'wb') as outfile:
 			json.dump(city_array,outfile,indent=4)
-
+biggest_array = []
 with open(output) as second_step_data:
 	load_data_info = json.load(second_step_data)
+	empty_array = {}
 	for info in load_data_info:
 		main_wrapper_json = info['business_information:']['businesses']
 		for info in main_wrapper_json:
 			url =  info['mobile_url']
 			soup = BeautifulSoup(requests.get(url).text)
+			try:
+			
+				business_name = soup.find('h1', attrs = {'class':'biz-page-title embossed-text-white'}).text.replace('\n','').replace('\u2019','')
+			
+			except AttributeError:
+				continue
+			
+
 			today_information = soup.findAll('ul', attrs = {'class':'iconed-list'})#BUSINESS TODAY INFO.
 			for hour_range in today_information:
 				hour_info = hour_range.findAll('span', attrs = {'class':'hour-range'})
 				price_info = hour_range.findAll('dd', attrs = {'class':'nowrap price-description'})
 				menu_hyper_link = hour_range.findAll('div', attrs = {'class':'iconed-list-story'})
-
-
+				for hour_text,price_text in zip(hour_info,price_info):
+					first_dict = {}
+					first_dict['today_hour'] = hour_text.text.replace('\n','')
+					first_dict['price_range'] = price_text.text.replace('\n','')
+					biggest_array.append(first_dict)
 				for a_href in menu_hyper_link: #BELOW FROM THIS IS MENU SCRAPING
 					a_link = a_href.findAll('a')
 					for href in a_link:
@@ -55,10 +67,10 @@ with open(output) as second_step_data:
 							main_wrapper = second_request.find('div', attrs = {'class':'menu-section'}).findAll('div',attrs = {'class':'menu-item-details'})
 							for name in main_wrapper:
 								try:
-									name_info = name.find('h3').text
-									item_description = name.find('p', attrs = {'class':'menu-item-details-description'}).text
-									item_price_info = name.find('li', attrs = {'class':'menu-item-price-amount'}).text
-									#print item_price_info
+									second_dict = {}
+									second_dict['item_name'] = name.find('h3').text.replace('\n','')
+									second_dict['item_description'] = name.find('p', attrs = {'class':'menu-item-details-description'}).text.replace('\n','')
+									biggest_array.append(second_dict)
 								except AttributeError:
 									continue
 
@@ -68,12 +80,22 @@ with open(output) as second_step_data:
 			biz_website = soup.findAll('div',attrs = {'class':'biz-website'})
 			for a_biz in biz_website:
 				get_me_all_link = a_biz.findAll('a')
-				# for text in get_me_all_link:
-				# 	print text.text
-			total_ratings = soup.find('span', attrs = {'class':'review-count rating-qualifier'}).text
-			
+				for text in get_me_all_link:
+					third_dict = {}
+					third_dict['business_url'] = text.text.replace('\n','')
+					biggest_array.append(third_dict)
+			fourth_dict = {}		
+			fourth_dict['total_rating_for_this_business'] = soup.find('span', attrs = {'class':'review-count rating-qualifier'}).text.replace('\n','')
+			biggest_array.append(fourth_dict)
 			opening_information = soup.findAll('div', attrs = {'class':'ywidget biz-hours'})#OPENING HOURS IN TOTAL	
-			# for opening_info in opening_information:
-			#  	print opening_info.text.replace('Edit business info', ' ')
+			for opening_info in opening_information:
+				fifth_dict = {}
+			 	fifth_dict['Hours'] = opening_info.text.replace('Edit business info', ' ').replace('\n','')
+			 	biggest_array.append(fifth_dict)
+
+				empty_array[business_name] = biggest_array
+			
+			with open('parsed_data.json','wb') as outfile:
+				json.dump(empty_array,outfile,indent=4)	 	
 #if __name__ == "__main__":
 	
