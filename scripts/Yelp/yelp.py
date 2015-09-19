9478c9207e6a675805762bb5639402525d9bdade
Ben Dahal <Ben@Samirs-MacBook-Pro.local>
2015-09-19 10:51:14 -0700

    Added yelp script

--- scripts/Yelp/yelp.py
+++ scripts/Yelp/yelp.py
@@ -4,10 +4,9 @@ consumer_key = 'J6G3zEBCox3lUfKCcJBLDA'
 consumer_secret = 'oXcvu0rV8NFvSMoRgf_FGZf-72Y'
 token = 'So3cdsVQ-4zB0AAD8MF9VnuDF7XFsihw'
 token_secret = 'PAi0q1eI-lFMI9Az0ZWQ_qEFQ_k'
-latitude = 37.870
-longitude = 122.259
+location = 'UC BERKELEY'
 yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
-search_results = yelp_api.search_query(cll=[latitude,longitude])
+search_results = yelp_api.search_query(location = location)
 with open('yelp_business.json','wb') as outfile:
 	json.dump(search_results,outfile,indent=4)
 print search_results
