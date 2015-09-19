8ed3c89120ff26eb3ac609f93bdfeff477b712f6
Ben Dahal <Ben@Samirs-MacBook-Pro.local>
2015-09-19 11:50:30 -0700

    Loops thru all the popular schools and returns us the result

--- scripts/Yelp/yelp.py
+++ scripts/Yelp/yelp.py
@@ -1,14 +1,35 @@
 from yelpapi import YelpAPI
 import json
+
+
+
+
+
 consumer_key = 'J6G3zEBCox3lUfKCcJBLDA'
 consumer_secret = 'oXcvu0rV8NFvSMoRgf_FGZf-72Y'
 token = 'So3cdsVQ-4zB0AAD8MF9VnuDF7XFsihw'
 token_secret = 'PAi0q1eI-lFMI9Az0ZWQ_qEFQ_k'
-location = 'UC BERKELEY'
 yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
-search_results = yelp_api.search_query(location = location)
-with open('yelp_business.json','wb') as outfile:
-	json.dump(search_results,outfile,indent=4)
-print search_results
-# business_results = yelp_api.business_query(id=business_id, other_args)
-# phone_search_results = yelp_api.phone_search_query(phone=phone_number, other_args)
+
+
+city_info = []
+with open('fa15_final.json') as data_file:
+	data_info = json.load(data_file)
+	for city in data_info:
+		cities = city['city']
+		city_info.append(cities)
+
+
+def get_school_data_information(city_info):
+	city_array = []
+	for loop_cities in city_info[0:5]:
+		dictionary = {}
+		search_results = yelp_api.search_query(location = loop_cities)
+		dictionary['business_information:'] = search_results
+		city_array.append(dictionary)
+		with open('yelp_school_data','wb') as outfile:
+			json.dump(city_array,outfile,indent=4)
+
+
+if __name__ == "__main__":
+	get_school_data_information(city_info)
