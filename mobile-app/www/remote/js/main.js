6f23f886619c280158cc9d334db54ed253292f02
Gabrielle Wee <mail@gabriellew.ee>
2015-09-22 11:24:13 -0700

    fixed responsiveness on access page

--- mobile-app/www/remote/js/main.js
+++ mobile-app/www/remote/js/main.js
@@ -20,7 +20,7 @@ if (LOCAL) {
   //BASE_URL = 'http://192.168.42.124:8100';
   // BASE_URL = 'http://192.168.1.43:8100';
 
-  REST_URL = "http://localhost:5000"
+  // REST_URL = "http://localhost:5000"
   // REST_URL = 'https://www.uguru.me'
   //BASE_URL = 'http://192.168.43.155:8100';
 
