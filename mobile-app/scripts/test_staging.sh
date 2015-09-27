6ce36f7afbdba0a28a0514d14db875f839f3b6c5
Samir Makhani <sam1rm@berkeley.edu>
2015-09-27 00:34:25 -0700

    {pushing to staging environment before production}

---
+++ mobile-app/scripts/test_staging.sh
@@ -0,0 +1,66 @@
+### pp=prepare_production
+### up=update_production
+### fp=force_production
+### lp=local_production
+### sp=staging_production
+### ps=prepare_staging
+### us=update_staging
+### rid=run_ios_device
+### rad=run_android_device
+### ridl=run_ios_device_reload
+### radl=run_android_device_reload
+
+source ~/.bash_profile
+echo "Clearing iOS xCode cache..."
+rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache"
+echo 'cd to uguru-mobile repository'
+um
+echo '================'
+echo '=============='
+echo '==========='
+echo
+echo 'compressing ..... ..... .... .    ....'
+echo
+echo 'compressing ..... ..... .... .    ...'
+echo
+echo 'compressing ..... ..... .... .    ..'
+echo
+echo 'compressing ..... ..... .... .    .'
+echo
+echo 'compressing ..... ..... .... .    '
+echo
+echo 'compressing ..... ..... .... .    .'
+echo
+echo 'compressing ..... ..... .... .    ..'
+echo
+echo 'compressing ..... ..... .... .    ...'
+echo
+echo 'compressing ..... ..... .... .    ....'
+echo
+echo 'minifying + compressing local assets..'
+
+gulp -b
+echo 'compiling all local assets..'
+cp dest/scripts/*.js www/remote/js/app.js && cp dest/styles/*.css www/remote/css/app_version.css
+echo 'cd to uguru-web'
+cd ..
+echo 'copying new files from uguru-mobile'
+cp -r mobile-app/www/remote/* app/static/remote/
+echo 'updating ios plugins'
+# cp mobile-app/platforms/ios/www/cordova.js app/static/remote/ios.cordova.js && cp mobile-app/platforms/ios/www/cordova_plugins.js app/static/remote/ios.cordova_plugins.js
+echo
+echo
+echo
+echo
+echo
+echo 'Try opening localhost:5000/static/remote/index.html'
+echo
+echo
+echo
+echo
+echo 'jk -- we can do that for you too ;)'
+echo
+echo
+echo
+open 'http://localhost:5000/static/remote/index.html'
+
