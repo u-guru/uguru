f580bac5a3a38bf25f6a6128a42c59d83e456b7e
Gabrielle Wee <mail@gabriellew.ee>
2015-10-01 19:49:33 -0700

    fixed signup/login

--- mobile-app/www/remote/js/controllers/student/home.ctrl.js
+++ mobile-app/www/remote/js/controllers/student/home.ctrl.js
@@ -93,7 +93,7 @@ angular.module('uguru.student.controllers', [])
                 $scope.root.vars.detailed_verbs_index_clicked = verb_index;
             }
 
-            $ionicModal.fromTemplateUrl(BASE + 'templates/availability.modal.html', {
+            $ionicModal.fromTemplateUrl(BASE + 'templates/request.modal.html', {
                 scope: $scope,
                 animation: 'slide-in-up'
             }).then(function(modal) {
