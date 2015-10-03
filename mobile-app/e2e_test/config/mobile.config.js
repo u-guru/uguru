78082f27237bae15552c63a7757face2e7bb5ab2
samir makhani <sam1rm@berkeley.edu>
2015-10-02 23:57:54 -0700

    added to git ignore

--- mobile-app/e2e_test/config/mobile.config.js
+++ mobile-app/e2e_test/config/mobile.config.js
@@ -1,3 +1,14 @@
+
+// iOS DEVICE ONLY
+// 0. PREREQS
+// --- Make sure Xcode is turned off
+// 1. Live reload device functioning
+// 2. Inside of this file, make sure UUIDs are the one on the phone
+// 3. Appium on the terminal 
+//    --> In Terminal: appium -U UUID
+// 4. [SKIP ANDROID] ios_webkit_debug_proxy -c 06108c5ea66a640ffdd5e9902c036a26d6af4c0f:27753 -d
+// 5. [SKIP ANDROID] Turn on instruments & press record
+
 exports.config = {
     framework: 'jasmine2',
     //appium port has to be 4723
@@ -10,17 +21,14 @@ exports.config = {
 //appium -U <uuid< --app <app_path>
 //0df70c3eacd748d8d92a882cbec7b26787fa0396
 
-//ios_webkit_debug_proxy -c 0df70c3eacd748d8d92a882cbec7b26787fa0396:27753 -d
-//appium -U 0df70c3eacd748d8d92a882cbec7b26787fa0396
-          //ios settings
                  browserName: '',
                  'appium-version': '1.4.11',
-                 app: '/Users/nlmac/Git/uguru/mobile-app/platforms/ios/build/device/uguru.app',
+                 app: '/Users/Samir/Git/uguru/mobile-app/platforms/ios/build/device/uguru.app',
                  platformName: 'iOS',
                   bundleId: 'com.beta.college.Uguru',
-                  udid: '0df70c3eacd748d8d92a882cbec7b26787fa0396',
-                  platformVersion: '7.12',
-                   deviceName: 'TwFoB Work’s iPhone (7.1.2) [0df70c3eacd748d8d92a882cbec7b26787fa0396]',
+                  udid: '06108c5ea66a640ffdd5e9902c036a26d6af4c0f',
+                  platformVersion: '8.1.2',
+                   deviceName: 'TwFoB Work’s iPhone (8.1.2) [06108c5ea66a640ffdd5e9902c036a26d6af4c0f]',
                    autoWebview: 'true'
 
          // //android settings
@@ -31,10 +39,11 @@ exports.config = {
                 //  platformVersion: '4.4',
                 //   deviceName: 'S5',
                 //   autoWebview: 'true'
-           
+
          },
          // comment this out if running for IOS
          //baseUrl: 'http://10.0.2.2:8000',
+
 
          // this is for ios
         baseUrl: 'http://localhost:8100',
@@ -94,11 +103,14 @@ exports.config = {
             // browser.driver.manage().window().setSize(414, 736);
 
             //may need to uncomment this for IOS. but for sure comment out when on android
+
+               // browser.driver.get("http://localhost:8100/#/");
+
                //browser.driver.get("http://localhost:8100/#/");
               // browser.sleep(1000);
 
 
 
         }
-        
+
 };
