79f01de3f96d40dc7b60b26acae3a74cd545607b
Samir Makhani <sam1rm@berkeley.edu>
2015-10-03 17:19:39 -0700

    gabrielle guru ranking implemented

--- app/static/remote/js/controllers/student/home.ctrl.js
+++ app/static/remote/js/controllers/student/home.ctrl.js
@@ -18,10 +18,11 @@ angular.module('uguru.student.controllers', [])
     '$ionicActionSheet',
     '$ionicPopover',
     'uTracker',
+    'MapService',
     function($scope, $state, $ionicPlatform, $cordovaStatusbar,
         $ionicModal, $timeout, $q, University, $localstorage,
         $ionicSideMenuDelegate, $ionicBackdrop, $ionicViewSwitcher,
-        $ionicActionSheet, $ionicPopover, uTracker) {
+        $ionicActionSheet, $ionicPopover, uTracker, MapService) {
 
         $ionicSideMenuDelegate.canDragContent(true);
 
@@ -92,7 +93,7 @@ angular.module('uguru.student.controllers', [])
                 $scope.root.vars.detailed_verbs_index_clicked = verb_index;
             }
 
-            $ionicModal.fromTemplateUrl(BASE + 'templates/availability.modal.html', {
+            $ionicModal.fromTemplateUrl(BASE + 'templates/request.modal.html', {
                 scope: $scope,
                 animation: 'slide-in-up'
             }).then(function(modal) {
@@ -168,16 +169,59 @@ angular.module('uguru.student.controllers', [])
             }
         }
 
+        $scope.initStudentHomeMap = function() {
+            var icon = {
+                 title: "Guru Profile",
+                 fillColor: "#c21f31",
+                 fillOpacity: 0.8,
+                 path: "M0-165c-27.618 0-50 21.966-50 49.054C-50-88.849 0 0 0 0s50-88.849 50-115.946C50-143.034 27.605-165 0-165z",
+                 anchor: new google.maps.Point(0,0),
+                 strokeWeight: 0,
+                 scale: 1/4,
+                 latitude: $scope.user.university.latitude,
+                 longitude: $scope.user.university.longitude,
+                 labelClass:'student-home-map-label'
+            }
+
+            var mapOptions = {
+                "latitude": $scope.user.university.latitude,
+                "longitude": $scope.user.university.longitude,
+                "saturation": -100,
+                "elemId": "student-home-map-container",
+                "lightness": -5,
+                "zoom": 15,
+                "infoWindowHtml": "<span>Guru Profile would go here</span>",
+                "scrollwheel":false,
+                "icons": [icon],
+                "title": "Student Home Map",
+                styles: defaultMapStyles //see lib/angular-google-maps/default-map-styles.js
+            }
+
+            MapService.initMap(mapOptions);
+        }
 
 
         $scope.$on('$ionicView.loaded', function() {
 
             $scope.root.vars.guru_mode = false;
+            if (!$scope.mapInitialized) {
+                $scope.mapInitialized = true;
+                $timeout(function() {
+                    $scope.initStudentHomeMap();
+                }, 1000)
+            }
 
         })
 
         $scope.$on('$ionicView.enter', function() {
             $scope.loader.hide();
+
+            if (!$scope.mapInitialized) {
+                $scope.mapInitialized = true;
+                $timeout(function() {
+                    $scope.initStudentHomeMap();
+                }, 1000)
+            }
             // $timeout(function() {
             //     checkOnboardingStatus();
             // }, 1000);
