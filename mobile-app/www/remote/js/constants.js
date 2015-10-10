e3b15882ef8b0bca007a7d5c1d1326797a647ba9
Gabrielle Wee <mail@gabriellew.ee>
2015-10-09 17:01:50 -0700

    merge

--- mobile-app/www/remote/js/constants.js
+++ mobile-app/www/remote/js/constants.js
@@ -1,8 +1,3 @@
 var _local = "true";
-<<<<<<< HEAD
-var _startpage = "bakery-guru-profile";
+var _startpage = "guru-profile";
 var _ipaddress = "192.168.42.78";
-=======
-var _startpage = "guru";
-var _ipaddress = "192.168.0.102";
->>>>>>> 5726cafabb7903a6840313c5305f59aa2d85ec69
