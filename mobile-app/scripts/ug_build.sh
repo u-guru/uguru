47731e0faf7b5aab4e5792586451a29c19fdc5da
Samir Makhani <sam1rm@berkeley.edu>
2015-10-13 00:39:47 -0700

    new changes

--- mobile-app/scripts/ug_build.sh
+++ mobile-app/scripts/ug_build.sh
@@ -1,6 +1,6 @@
 
 if [ $# == 0 ];
-	then 
+	then
 	printf "\e[0;36m\n"
 	printf "ug build <android|ios> : Installs the specified platform along with required plugins.\n"
 	printf "\e[0m\n"
@@ -18,28 +18,28 @@ if [ $# == 1 ];
 		cordova platform rm ios
 		cordova platform rm android
 		rm -rf plugins/*
-		cp -R plugins_android/general/. plugins/
+		# cp -R plugins_android/general/. plugins/
 
-		# cordova plugin add ionic-plugin-keyboard
-		# cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
-		# cordova plugin add cordova-plugin-camera
-		# cordova plugin add cordova-plugin-crosswalk-webview
-		# cordova plugin add cordova-plugin-device
-		# cordova plugin add cordova-plugin-dialogs
-		# cordova plugin add cordova-plugin-inappbrowser
-		# cordova plugin add cordova-plugin-network-information
-		# cordova plugin add cordova-plugin-statusbar
-		# cordova plugin add cordova-plugin-whitelist
-		# cordova plugin add cordova-plugin-file@1.3.3
-		# cordova plugin add cordova-plugin-file-transfer@0.5.0
-		# cordova plugin add cordova-plugin-geolocation
-		# cordova plugin add cordova-plugin-splashscreen@1.0.0
+		cordova plugin add ionic-plugin-keyboard
+		cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
+		cordova plugin add cordova-plugin-camera
+		cordova plugin add cordova-plugin-crosswalk-webview
+		cordova plugin add cordova-plugin-device
+		cordova plugin add cordova-plugin-dialogs
+		cordova plugin add cordova-plugin-inappbrowser
+		cordova plugin add cordova-plugin-network-information
+		cordova plugin add cordova-plugin-statusbar
+		cordova plugin add cordova-plugin-whitelist
+		cordova plugin add cordova-plugin-file@1.3.3
+		cordova plugin add cordova-plugin-file-transfer@0.5.0
+		cordova plugin add cordova-plugin-geolocation
+		cordova plugin add cordova-plugin-splashscreen@1.0.0
 
 
 		cordova platform add android@4.1
-		cordova plugin add ./plugins_android/specific/phonegap-facebook-plugin-master --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"	
+		cordova plugin add ./plugins_android/specific/phonegap-facebook-plugin-master --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
 		cordova build android
-		
+
 	fi
 
 	if [ $platform == "ios" ]; then
@@ -47,24 +47,24 @@ if [ $# == 1 ];
 		cordova platform rm android
 		cordova platform rm ios
 		rm -rf plugins/*
-		cp -R plugins_ios/general/. plugins/
+		# cp -R plugins_ios/general/. plugins/
 
-		# cordova plugin add ionic-plugin-keyboard
-		# cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
-		# cordova plugin add cordova-plugin-camera
-		# cordova plugin add cordova-plugin-device
-		# cordova plugin add cordova-plugin-dialogs
-		# cordova plugin add cordova-plugin-inappbrowser
-		# cordova plugin add cordova-plugin-network-information
-		# cordova plugin add cordova-plugin-statusbar
-		# cordova plugin add cordova-plugin-whitelist
-		# cordova plugin add cordova-plugin-file@1.3.3
-		# cordova plugin add cordova-plugin-file-transfer@0.5.0
-		# cordova plugin add cordova-plugin-geolocation
-		# cordova plugin add cordova-plugin-splashscreen@1.0.0
+		cordova plugin add ionic-plugin-keyboard
+		cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
+		cordova plugin add cordova-plugin-camera
+		cordova plugin add cordova-plugin-device
+		cordova plugin add cordova-plugin-dialogs
+		cordova plugin add cordova-plugin-inappbrowser
+		cordova plugin add cordova-plugin-network-information
+		cordova plugin add cordova-plugin-statusbar
+		cordova plugin add cordova-plugin-whitelist
+		cordova plugin add cordova-plugin-file@1.3.3
+		cordova plugin add cordova-plugin-file-transfer@0.5.0
+		cordova plugin add cordova-plugin-geolocation
+		cordova plugin add cordova-plugin-splashscreen@1.0.0
 
 		cordova platform add ios@3.8
-		cordova plugin add ./plugins_ios/specific/phonegap-facebook-plugin-master --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"	
+		cordova plugin add ./plugins_ios/specific/phonegap-facebook-plugin-master --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
 		printf '\e[0;36mOpening xCode project and giving back control... \e[0m\n'
 		printf '\e[0;36mMake sure to set the correct signing identity and build with xCode. \e[0m\n'
 		printf '\e[0;36mFor xCode 7+: Make sure to disable "ENABLE_BITCODE" in build settings. \e[0m\n'
