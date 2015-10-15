0b06966127b72d533112f4d87b3f4e958870c2ce
Samir Makhani <sam1rm@berkeley.edu>
2015-10-14 22:52:58 -0700

    most updated changes

--- mobile-app/www/remote/js/constants.js
+++ mobile-app/www/remote/js/constants.js
@@ -1,4 +1,4 @@
 var _local = "true";
-var _startpage = "home";
+var _startpage = "cards";
 var _ipaddress = "192.168.0.115";
 
--- mobile-app/www/remote/js/controllers/util/CardList.ctrl.js
+++ mobile-app/www/remote/js/controllers/util/CardList.ctrl.js
@@ -23,6 +23,7 @@
     $scope.user.cards = [{card_last4:'2121',card_type:'Visa', id:1}, {card_last4:'3121',card_type:'Discover', id:2}, {card_last4:'4121',card_type:'Mastercard', id:3}]
     $scope.user.name = 'Samir Makhani';
 
+    $scope.editCard = {card_number: '', id:0, card_type:'', exp_date:''};
 
     $scope.$on('$ionicView.enter', function() {
 
@@ -39,9 +40,44 @@
             CardService.instatiateAllCards($scope.user.cards);
           }, 1000)
       }
-
-
     })
+
+    $scope.editCurrentCard = function(card) {
+      $scope.editCard = card;
+      $scope.editCard.card_number = '**** **** **** ' + card.card_last4;
+      $scope.editCard.exp_date = '** / **';
+      $scope.launchEditCardModal(card);
+    }
+
+
+     $ionicModal.fromTemplateUrl(BASE + 'templates/add-card.modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+      }).then(function(modal) {
+          $scope.editCardModal = modal;
+      });
+
+
+      $scope.launchEditCardModal = function(card) {
+        $scope.editCard = card || $scope.editCard;
+        $scope.editCardModal.show();
+      }
+
+      $scope.$on('modal.shown', function() {
+        $timeout(function() {
+
+
+          cardObj = CardService.userCardObj($scope.user.name, $scope.editCard.card_last4, $scope.editCard.id, $scope.editCard.card_type)
+          CardService.initCardView('card-modal-wrapper', cardObj);
+
+          if ($scope.editCard.id) {
+            $timeout(function() {
+              CardService.instatiateAllCards([$scope.editCard]);
+            }, 500)
+          }
+
+        }, 100);
+      });
 
   }
 
