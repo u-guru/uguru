639b841e34d09a811bc20a7b3904ac6694050a5d
nicknaky <nicknaky@users.noreply.github.com>
2015-10-21 17:27:58 -0700

    updated ios scripts

--- mobile-app/scripts/ug_build.sh
+++ mobile-app/scripts/ug_build.sh
@@ -15,11 +15,9 @@ if [ $# == 1 ];
 
 	if [ $platform == 'android' ]; then
 		printf "\e[0;36mBuilding for $platform \e[0m\n"
-		# cordova platform rm ios
-		cordova platform rm android
-		rm -rf plugins/com.phonegap.plugins.facebookconnect
+		cordova platform rm ios
+		cordova platform rm android		
 		rm -rf plugins/*
-		# cp -R plugins_android/general/. plugins/
 
 		cordova plugin add ionic-plugin-keyboard
 		cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
@@ -37,37 +35,24 @@ if [ $# == 1 ];
 		cordova plugin add cordova-plugin-splashscreen@1.0.0
 
 		cordova plugin add cordova-plugin-keepe-cardio
-
 		cordova plugin add cordova-plugin-x-socialsharing
 		cordova plugin add cordova-plugin-calendar
-
-		
-		# cordova plugin add de.appplant.cordova.plugin.badge
 		cordova plugin add cordova-plugin-appavailability
-		cordova plugin add https://github.com/nicknaky/de.appplant.cordova.plugin.badge
-
-		# rm -f ./plugins_android/specific/phonegap-plugin-push/src/android/libs/gcm.jar
-		# rm -f ./plugins_android/specific/phonegap-facebook-plugin-master/platforms/android/FacebookLib/libs/android-support-v4
-		# # cp -f ./plugins_android/specific/android-support-v13.jar ./plugins_android/specific/phonegap-plugin-push/src/android/libs/
-		# cp -f ./plugins_android/specific/android-support-v13.jar ./plugins_android/specific/phonegap-facebook-plugin-master/platforms/android/FacebookLib/libs/
 		
-		# cp -f ./plugins_android/specific/fb_settings/AndroidManifest.xml ./plugins_android/specific/phonegap-facebook-plugin-master/platforms/android/FacebookLib/
-		# cp -f ./plugins_android/specific/fb_settings/build.gradle ./plugins_android/specific/phonegap-facebook-plugin-master/platforms/android/FacebookLib/
-		# cp -f ./plugins_android/specific/fb_settings/project.properties ./plugins_android/specific/phonegap-facebook-plugin-master/platforms/android/FacebookLib/
-
-		# cp -f ./plugins_android/specific/push_settings/plugin.xml ./plugins_android/specific/phonegap-plugin-push/plugin.xml
-
+		cordova plugin add https://github.com/Uguru/de.appplant.cordova.plugin.badge
 
 		cordova platform add android@4.1
 		printf "\e[0;36mInstalling and configuring the Android Support Libray v13 for Facebook and Push plugins... \e[0m\n"
 
-		# cordova plugin add ./plugins_android/specific/phonegap-plugin-push
-		cordova plugin add https://github.com/nicknaky/phonegap-plugin-push
-
+		cordova plugin add https://github.com/Uguru/phonegap-plugin-push
+		cordova plugin add https://github.com/Uguru/phonegap-facebook-plugin.git --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
 		
+<<<<<<< HEAD
 		# cordova plugin add ./plugins_android/specific/phonegap-facebook-plugin --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
 		cordova plugin add https://github.com/nicknaky/phonegap-facebook-plugin.git --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
 
+=======
+>>>>>>> 0c8e3e5... updated ios scripts
 		# cp plugins_android/specific/build-extras.gradle platforms/android/
 
 		cordova build android
@@ -79,27 +64,35 @@ if [ $# == 1 ];
 		cordova platform rm android
 		cordova platform rm ios
 		rm -rf plugins/*
-		# cp -R plugins_ios/general/. plugins/
-
+		
 		cordova plugin add ionic-plugin-keyboard
-		cordova plugin add ~/Git/packages/phonegap-facebook-plugin-master --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
-		# cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
+		cordova plugin add https://github.com/Telerik-Verified-Plugins/NativePageTransitions
 		cordova plugin add cordova-plugin-camera
 		cordova plugin add cordova-plugin-device
 		cordova plugin add cordova-plugin-dialogs
 		cordova plugin add cordova-plugin-inappbrowser
 		cordova plugin add cordova-plugin-network-information
 		cordova plugin add cordova-plugin-statusbar
-		cordova plugin add cordova-plugin-whitelist
+		# cordova plugin add cordova-plugin-whitelist
 		cordova plugin add cordova-plugin-file@1.3.3
 		cordova plugin add cordova-plugin-file-transfer@0.5.0
 		cordova plugin add cordova-plugin-geolocation
 		cordova plugin add cordova-plugin-splashscreen@1.0.0
 
+		cordova plugin add cordova-plugin-keepe-cardio
+		cordova plugin add cordova-plugin-x-socialsharing
+		cordova plugin add cordova-plugin-calendar
+		cordova plugin add cordova-plugin-appavailability
+
+		cordova plugin add https://github.com/Uguru/de.appplant.cordova.plugin.badge
+
 		cordova platform add ios@3.8
-		# NICK -- plz leave this here for me!
-		cordova plugin add ~/Git/packages/phonegap-facebook-plugin-master --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
-		# cordova plugin add /Users/nlmac/Git/uguru/mobile-app/plugins_ios/specific/phonegap-facebook-plugin/ --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
+		
+		printf "\e[0;36mInstalling and configuring the Android Support Libray v13 for Facebook and Push plugins... \e[0m\n"
+
+		cordova plugin add https://github.com/Uguru/phonegap-plugin-push
+		cordova plugin add https://github.com/Uguru/phonegap-facebook-plugin.git --variable APP_ID="1416375518604557" --variable APP_NAME="Uguru"
+
 		printf '\e[0;36mOpening xCode project and giving back control... \e[0m\n'
 		printf '\e[0;36mMake sure to set the correct signing identity and build with xCode. \e[0m\n'
 		printf '\e[0;36mFor xCode 7+: Make sure to disable "ENABLE_BITCODE" in build settings. \e[0m\n'
