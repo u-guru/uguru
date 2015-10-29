34858d414302a7bb3317032f37412203f6df4936
nicknaky <nicknaky@users.noreply.github.com>
2015-10-29 15:10:52 -0700

    added loadingService

--- mobile-app/www/remote/js/shared/LoadingService.js
+++ mobile-app/www/remote/js/shared/LoadingService.js
@@ -1,29 +1,77 @@
 angular
 	.module('sharedServices')
-	.factory('LoadingService', ['$ionicLoading', LoadingService]);
+	.factory('LoadingService', [
+		'$scope',
+		'$ionicLoading',
+		LoadingService]);
 
-function LoadingService($ionicLoading) {
+function LoadingService($scope, $ionicLoading) {
 
 	return {
 		show: show,
+		showAmbig: showAmbig,
+		showFailure: showFailure,
+		showSuccess: showSuccess,
+		updateSuccessText: updateSuccessText,
 		hide: hide
-	}
+	};
 
-	function show(delay, duration, message) {
-		if(!message) {
-			message = 'Saved!';
-		}
-		console.log("ionicLoading.show()");
-		$ionicLoading.show({
-	 		//template: '<span id="E2E-msg" value="' + message + '" class="capitalized">'  + message + '</span>',
-	        template: message,
-	        delay: delay,
-	        duration: duration
-		});
-	}
+    function show() {
+        $ionicLoading.show({
+            templateUrl: BASE + 'templates/u.loader.ambiguous.svg.html'
+        });
+    }
 
-	function hide() {
-		$ionicLoading.hide();
-	}
+    function showAmbig(text, duration) {
+        $scope.ambigLoaderText = text || '';
+
+        $ionicLoading.show({
+            scope: $scope,
+            templateUrl: BASE + 'templates/u.loader.ambiguous.svg.html',
+            duration: duration || 1000
+        });
+        
+    }
+
+    function showFailure(text, duration) {
+        $scope.ambigLoaderText = text || '';
+
+        $ionicLoading.show({
+            scope: $scope,
+            templateUrl: BASE + 'templates/u.loader.failure.svg.html',
+            duration: duration || 1000
+        });
+        
+    }
+
+    function showSuccess(text, duration, callback) {
+
+        $scope.successLoaderText = text || '';
+
+        $ionicLoading.show({
+            scope: $scope,
+            templateUrl: BASE + 'templates/u.loader.success.svg.html',
+            duration: duration || 1000
+        });
+        
+        if (typeof callback !== 'undefined') {
+    		callback();
+        } 
+    }
+
+    function updateSuccessText(text) {
+        $scope.successLoaderText = text || 'loading';
+    }
+
+    function hide(delay) {
+        $scope.ambigLoaderText = '';
+        delay = delay || 0;
+        $timeout(function() {
+            $ionicLoading.hide();
+            
+        }, delay);
+    }
+
+
 
 }
