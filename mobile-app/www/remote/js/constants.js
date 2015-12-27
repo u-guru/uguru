5ad347f8bbd7cf18e04ef028b29400c29cf2c238
Jason Huang <f230536@hotmail.com>
2015-12-26 16:14:29 -0800

    add name for test

--- mobile-app/www/remote/js/constants.js
+++ mobile-app/www/remote/js/constants.js
@@ -1,5 +1,5 @@
 var _local = "true";
-var _startpage = "essay-student-home-desktop";
+var _startpage = "essay-home";
 // var _startpage = "essay-student-university";
 var _ipaddress = "192.168.0.102";
 var _autoredirects = "false";
