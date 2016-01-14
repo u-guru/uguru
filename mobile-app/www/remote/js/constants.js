ca1d89fc9dd9f69395b139e909189787b4885824
Gabrielle Wee <mail@gabriellew.ee>
2016-01-13 19:54:14 -0800

    university specific update

--- mobile-app/www/remote/js/constants.js
+++ mobile-app/www/remote/js/constants.js
@@ -1,4 +1,4 @@
 var _local = "true";
-var _startpage = "guru-home";
-var _ipaddress = "10.90.48.77";
+var _startpage = "university/2307";
+var _ipaddress = "192.168.42.120";
 var _autoredirects = "false"
