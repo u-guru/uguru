3cc6d7781d6d9ac4cb37683e17620ceb90501c06
Gabrielle Wee <mail@gabriellew.ee>
2016-03-18 19:01:43 -0700

    update

--- mobile-app/www/remote/js/controllers/util/util.admin.js
+++ mobile-app/www/remote/js/controllers/util/util.admin.js
@@ -19,8 +19,8 @@ angular.module('uguru.util.controllers')
 			layouts: AdminContent.getLayouts(),
 			user_stories: AdminContent.getUserStories(),
 			defaults: {
-				tabsIndex: 0,
-				sidebarIndex: 1
+				tabsIndex: 2,
+				sidebarIndex: 2
 			}
 		}
 		$scope.selected_component = $scope.page.components[4];
