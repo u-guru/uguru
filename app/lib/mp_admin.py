4a3e53f19e02d93931167206a57a94e141fb3e36
Samir Makhani <sam1rm@berkeley.edu>
2016-03-20 05:27:05 -0700

    most updated

--- app/lib/mp_admin.py
+++ app/lib/mp_admin.py
@@ -4,8 +4,9 @@ import requests
 mp = Mixpanel('28635453906d5f7d1a0383e6da3292df', 'd557efd7196433033a7a136fa016ef59', '3e9825a431a4944b5037c7bf6d909690')
 
 def createAdminProfiles():
-    teamNames = ['Samir', 'Jeselle', 'Gabrielle', 'Jason']
-    teamLastNames = ['Makhani', 'Obina', 'Wee', 'Huang']
+    mp = Mixpanel('3e9825a431a4944b5037c7bf6d909690')
+    teamNames = ['Samir', 'Jeselle', 'Gabrielle', 'Jason', 'Base']
+    teamLastNames = ['Makhani', 'Obina', 'Wee', 'Huang', 'Component']
     index = 0
     for person in teamNames:
         mp.people_set(str(index), {
@@ -16,22 +17,26 @@ def createAdminProfiles():
             })
         index += 1
 
-def getAdminProfiles():
+def getQueryDict(url, params={}):
     import urllib, urllib2, time #for sending requests
-    params = {}
-    params['api_key']= '28635453906d5f7d1a0383e6da3292df'
+    base_params = {}
+    base_params['api_key']= '28635453906d5f7d1a0383e6da3292df'
     # params['api_secret'] = 'd557efd7196433033a7a136fa016ef59'
-    params['expire'] = int(time.time())+600 # 600 is ten minutes from now
-    if 'sig' in params: del params['sig']
-    params['sig'] = hash_args(mp, params)
+    base_params['expire'] = int(time.time())+600 # 600 is ten minutes from now
+    base_params.update(params)
 
-    request_url = 'https://mixpanel.com/api/2.0/engage/?' + unicode_urlencode(params)
+    if 'sig' in base_params: del base_params['sig']
+    base_params['sig'] = hash_args(mp, base_params)
+
+    request_url = 'https://mixpanel.com/api/2.0/%s/?%s' % (url ,unicode_urlencode(base_params))
 
     request = requests.get(request_url).text
-
-    #print request_url
-
     return request
+
+def formatMPResponse(resp):
+    import json
+    arr = json.loads(resp)
+    return arr
 
 def unicode_urlencode(params):
     import urllib
@@ -74,9 +79,62 @@ def hash_args(self, args, secret=None):
 
         hash = hashlib.md5(args_joined)
         hash.update('d557efd7196433033a7a136fa016ef59')
-
         return hash.hexdigest()
 
-data = getAdminProfiles()
-from pprint import pprint
-pprint(data)
+
+def loadMostUpdatedElementsJson():
+    import json
+    _dict = json.load(open('./app/lib/elements.json'))
+    return _dict
+
+
+def initBaseComponent():
+    mp = Mixpanel('3e9825a431a4944b5037c7bf6d909690')
+    elements = loadMostUpdatedElementsJson()
+    # mp.track('4', elements)
+    mp.people_set('4', {
+        'elements': elements
+        })
+    # mp.people_append('4', elements)
+
+def updateElementComponent(elements):
+    mp = Mixpanel('3e9825a431a4944b5037c7bf6d909690')
+    mp.people_append('4', {'$properties': {}})
+
+def getMostUpdatedMPElements():
+    user = formatMPResponse(getQueryDict('engage', {'distinct_id': 4}))
+    elements =  user['results'][0]['$properties']['elements']
+    return elements
+
+
+def resolveElements():
+    import json
+    _dict = json.load(open('./app/lib/elements.json'))
+    return _dict
+
+def saveDictToElementsJson():
+    import json
+    _dict = getMostUpdatedElements()
+    with open('./app/lib/elements.json', 'wb') as fp:
+        json.dump(_dict, fp, sort_keys = True, indent = 4)
+    print "saved to elements.json"
+
+def resolveDictToElementsJson():
+    import json
+    master_elements_dict = json.load(open('./app/lib/elements.json'))
+    mp_elements_dict = getMostUpdatedMPElements()
+    return master_elements_dict == mp_elements_dict
+
+
+def addNewElement(element, _dict):
+    from elements import base_elements
+    mp_elements_dict = getMostUpdatedMPElements()
+
+    new_id = len(mp_elements_dict[element]) + 1
+    _dict['id'] = new_id
+    mp_elements_dict[element].append(_dict)
+    mp = Mixpanel('3e9825a431a4944b5037c7bf6d909690')
+    mp.people_set('4', {
+        'elements': mp_elements_dict
+    })
+
