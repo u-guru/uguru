c752fd35096c9a100cb76bfde4c74271ece7c717
Jason Huang <f230536@hotmail.com>
2016-04-07 10:20:17 -0700

    adding jason_gulp file

---
+++ mobile-app/jason_gulpfile.js
@@ -0,0 +1,131 @@
+'use strict';
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var beep = require('beepbeep');
+var express = require('express');
+var path = require('path');
+var open = require('open');
+var stylish = require('jshint-stylish');
+var connectLr = require('connect-livereload');
+var streamqueue = require('streamqueue');
+var runSequence = require('run-sequence');
+var merge = require('merge-stream');
+var minifyCSS = require('gulp-minify-css');
+var debug = require('gulp-debug');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+var karma = require('karma').server;
+var preprocess = require('gulp-preprocess');
+var replace = require('gulp-replace-task');
+var fs = require('fs');
+
+// var args = require('yargs').argv;
+var args = require('yargs')
+    .alias('uld', 'update-local-device')
+    .alias('b', 'build')
+    .default('build', false)
+    .argv;
+
+var build = args.build;
+var uld = args.uld;
+var targetDir = path.resolve('dest');
+
+var errorHandler = function(error) {
+  if (build) {
+    throw error;
+  } else {
+    beep(2, 170);
+    plugins.util.log(error);
+  }
+};
+
+var done = function()
+{
+	console.log("DONE")
+}
+
+//All the gulp task
+gulp.task('clean',function() {
+	console.log(targetDir)
+	del([targetDir]);
+});
+
+gulp.task('Copy Templates',function() {
+	return gulp.src([
+			'templates/*html'
+
+	      // 'templates/**/*.html',
+	      // 'templates/**/**/*.html',
+	      // 'templates/**/**/**/*.html',
+	      // 'templates/svg/**/*.html',
+	      // 'templates/student.home.html',
+	      // 'templates/student.home.body.html',
+	      // 'templates/student.guru-book.html',
+	      // 'templates/student.settings.html',
+	      // 'templates/student.settings.html',
+	      // 'templates/components/modals/add-course.modal.html',
+	      // 'templates/components/modals/add-university.modal.html',
+	      // 'templates/components/modals/university.modal.html',
+	      // // 'templates/components/modals/signup.modal.html',
+	      // 'templates/components/modals/become-guru.modal.html',
+	      // 'templates/util/offline.html',
+	      // 'templates/*html',
+	      // 'templates/dev/*html',
+	      // 'templates/elements/*',
+	      // 'templates/elements/**/**',
+	      // 'templates/elements/**/**/**',
+	      // 'templates/elements/**/**/**/**',
+	      // 'templates/elements/**/**/**/**/**',
+	      // 'templates/guru/*html',
+	      // 'templates/components/modals/*html',
+	      // 'templates/components/details/*html',
+	      // 'templates/components/inputs/*html',
+	      // 'templates/components/**/**/**.html',
+	      // 'templates/splash/*.html',
+	      // 'templates/splash/**/*.html',
+	      // 'templates/splash/**/**/**.html',
+	    ], { cwd: 'www/remote' })
+	  .pipe(gulp.dest(path.join(targetDir, 'templates')))
+	  .on('error', errorHandler);
+});
+
+gulp.task('styles',function() {
+	console.log("styles")
+});
+
+gulp.task('jsHint',function() {
+	console.log("jsHint")
+});
+
+gulp.task('scripts',function() {
+	console.log("scripts")
+});
+gulp.task('noop', function() {
+	console.log("SKIP")
+});
+
+gulp.task('watchers', function() {
+	console.log("watchers")
+});
+gulp.task('serve', function() {
+	console.log("serve")
+});
+
+gulp.task('default',function() {
+	runSequence(
+	  'clean',
+	  [
+	    'Copy Templates',
+	    'styles',
+	    'jsHint',
+	    'scripts'
+	  ],
+	  // // 'index',
+	  build ? 'noop' : ['watchers','serve'],
+	  done);
+});
+
+
