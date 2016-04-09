65998be1cb6c6f4ac24d2f2c7746f56aded83109
Gabrielle Wee <mail@gabriellew.ee>
2016-04-08 18:23:46 -0700

    update

--- mobile-app/www/remote/js/directives/component.js
+++ mobile-app/www/remote/js/directives/component.js
@@ -565,3 +565,10 @@ angular.module('uguru.components', [])
             replace: true
         }
     })
+    .directive('toast', function() {
+        return {
+            templateUrl: BASE + 'templates/elements/components/info/toast.tpl',
+            restrict: 'E',
+            replace: true
+        }
+    })
