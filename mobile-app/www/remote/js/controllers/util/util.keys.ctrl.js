72aef0b1f20dc668ed70141f4673ab5efec57a1f
Samir Makhani <sam1rm@berkeley.edu>
2016-04-15 18:35:35 -0700

    most updated

--- mobile-app/www/remote/js/controllers/util/util.keys.ctrl.js
+++ mobile-app/www/remote/js/controllers/util/util.keys.ctrl.js
@@ -7,25 +7,158 @@ angular.module('uguru.util.controllers')
 	'$stateParams',
 
 	function($scope, $state, $stateParams) {
+
+		var keyboardSpec = {
+			//toggle properties
+		}
+
+		$scope.player = function() {
+			return {
+				start: playDanceMoves,
+				pause: pauseDanceMoves,
+				setMode: {
+					options: [{name: "fast", speed: 250}, {name: "medium", speed:1000}, {name: "slow", "speed": 2000}],
+					selectedIndex: 0
+				},
+			}
+		}
+
+		function playDanceMoves(elem, dance_moves) {
+			for (var i = 0; i < dance_moves.length; i++) {
+				console.log('playing dance move #', i + 1);
+				var indexDanceMove = dance_moves[i]
+				execDanceMove(elem, indexDanceMove);
+			}
+		}
+
+		function execDanceMove(elem, dance_obj) {
+			console.log(dance_obj);
+			var animKeys = Object.keys(dance_obj);
+			for (var i = 0; i < animKeys.length; i++) {
+				var indexProperty = animKeys[i];
+				if (browserPrefix && browserPrefix.length) {
+					indexProperty = indexProperty[0].toUpperCase() + indexProperty.slice(1);
+				}
+				var elemStyleProperty = browserPrefix + indexProperty;
+				var perspective = "perspective("+dance_obj.transformPerspective +"px) ";
+				var translate = "translate3d("+dance_obj.translateX+"px, "+dance_obj.translateY+"px, "+dance_obj.translateZ+"px) ";
+				var scale = "scale3d("+dance_obj.scale3DX+", "+dance_obj.scale3DY+", "+dance_obj.scale3DZ+") ";
+				var rotate = "rotate3d("+dance_obj.rotate3DX+", "+dance_obj.rotate3DY+", "+dance_obj.rotate3DZ+", "+dance_obj.rotate3DAngle+"deg)";
+				var origin = dance_obj.originX+"% "+dance_obj.originY+"%"+" "+dance_obj.originZ+"%";
+				var csstext = browserPrefix + "transform: "+perspective+translate +scale + rotate + "; "
+					+ browserPrefix + "transform-origin: " + origin + "; "
+					+ browserPrefix + "transform-style: " + dance_obj.transformStyle+";"
+					+ "transform: "+perspective+translate +scale + rotate + "; "
+					+ "transform-origin: " + origin + "; ";
+					+ "transform-style: " + dance_obj.transformStyle+";";
+
+				elem.style.cssText = elem.style.cssText + csstext;
+			}
+		}
+
 		//key spec
 		//space - play
 		//ctrl + left, go back a time state
 		//ctrl + right arrow
+		// NOTE: The change to red signifies the start of
+
+		//todo apply a matrix transform
 
 
-
-		/*Playing related*/
-		//ctrl + r, go back to beginning
-		//double-tap delete, remove time state
-
-		/*Property related*/
-		//tab: select property
-		//enter: when property selected, lock it
-
-
-		function initTimeObj() {
-
+		//TODO import all webkit related animation results
+		//TODO -->
+		function findKeyframesRule(rule) {
+		    var ss = document.styleSheets;
+		    for (var i = 0; i < ss.length; ++i) {
+		        for (var j = 0; j < ss[i].cssRules.length; ++j) {
+		            if (ss[i].cssRules[j].type == window.CSSRule.WEBKIT_KEYFRAMES_RULE && ss[i].cssRules[j].name == rule) { return ss[i].cssRules[j]; }
+		        }
+		    }
+		    return null;
 		}
+
+		function initView() {
+			$scope.page = {actor: null}
+			$scope.page.actor = document.querySelector('#rect-svg');
+			$scope.danceMoves = [];
+			$scope.selectedDanceMove = initDanceMove($scope.page.actor);
+			$scope.danceMoves.push($scope.selectedDanceMove);
+			browserPrefix = getBrowserPrefix();
+		}
+
+
+		function getBrowserPrefix() {
+			var browserPrefix;
+			navigator.sayswho= (function(){
+			  var N = navigator.appName, ua = navigator.userAgent, tem;
+			  var M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+			  if(M && (tem = ua.match(/version\/([\.\d]+)/i))!= null) M[2] = tem[1];
+			  M = M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
+			  M = M[0];
+			  if(M == "Chrome") { browserPrefix = "webkit"; }
+			  if(M == "Firefox") { browserPrefix = "moz"; }
+			  if(M == "Safari") { browserPrefix = "webkit"; }
+			  if(M == "MSIE") { browserPrefix = "ms"; }
+			})();
+			return browserPrefix;
+
+
+			}
+
+
+
+
+
+		function initDanceMove(actor) {
+			var defaultDanceMove = function()
+			{
+				this.domElem = actor || "";
+				this.transformPerspective = 0;
+				this.translateX = 0;
+				this.translateY = 0;
+				this.translateZ = 0;
+				this.scale3DX = 1;
+				this.scale3DY = 1;
+				this.scale3DZ = 1;
+				this.rotate3DX = 0;
+				this.rotate3DY = 0;
+				this.rotate3DZ = 0;
+				this.rotate3DAngle = 0;
+				this.originX = 50;
+				this.originY = 50;
+				this.originZ = 50;
+				this.transformStyle = "preserve-3d";
+			};
+			return new defaultDanceMove();
+		}
+
+		function initKeyboardListeners() {
+			document.addEventListener('keydown', danceShortCutKeys);
+
+			function danceShortCutKeys(e) {
+				if (e.keyCode === 32)  {
+					playDanceMoves($scope.page.actor, $scope.danceMoves);
+				}
+
+				if (e.keyCode === 82)  {
+					$scope.page.actor.style[browserPrefix + 'Transform'] = '';
+				}
+			}
+		}
+
+		//TODO
+		// - previous transform state should be detected
+
+
+
+		function initAll() {
+			initView()
+			initKeyboardListeners();
+		}
+
+		var browserPrefix;
+		initAll();
+
 	}
 
 ]);
