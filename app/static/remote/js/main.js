b03fe9c31ac95d0ada0acc76952a6569bb31e57f
Samir Makhani <sam1rm@berkeley.edu>
2016-04-27 13:13:13 -0700

    most updated

--- app/static/remote/js/main.js
+++ app/static/remote/js/main.js
@@ -584,10 +584,15 @@ angular.module('uguru', ['ionic','ionic.utils', 'restangular', 'ngCordova',
         templateUrl: BASE + 'templates/guru.conversations.html'
   }).
   //GPA GURU
-  state('root.intro', {
-        url: '/gpa-intro',
-        templateUrl: BASE + 'templates/dev/gpa/university.html',
-        controller: 'AddUniversityCtrl'
+  state('root.gpa-title', {
+        url: '/gpa-title',
+        templateUrl: BASE + 'templates/dev/gpa/title.html',
+        controller: 'GPAController'
+  }).
+  state('root.gpa-welcome', {
+        url: '/gpa-welcome',
+        templateUrl: BASE + 'templates/dev/gpa/welcome.gpa.html',
+        controller: 'GPAController'
   }).
   state('root.gpa-demographic', {
         url: '/gpa-demographic',
