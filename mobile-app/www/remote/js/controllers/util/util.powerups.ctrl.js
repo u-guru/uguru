02eaa11c03673b6ead1fb29b5ae56966d4f10930
Gabrielle Wee <mail@gabriellew.ee>
2016-04-27 10:07:19 -0700

    update

--- mobile-app/www/remote/js/controllers/util/util.powerups.ctrl.js
+++ mobile-app/www/remote/js/controllers/util/util.powerups.ctrl.js
@@ -14,7 +14,12 @@ angular.module('uguru.util.controllers')
   function($scope, $state, $timeout, $localstorage, $ionicPlatform,
     $cordovaKeyboard, $ionicModal,$ionicTabsDelegate) {
 
-
+        $scope.onMouseLeave = function(event) {
+                    var element = event.target;
+                    element.style.transform = "none";
+                    element.style.webkitTransform = "none";
+                    element.classList.add('transition-150');
+                }
 
   }
 
