1ba2072357bc2eb3f79d945336c5eb2137f7c6c4
Jason Huang <f230536@hotmail.com>
2016-05-16 16:10:03 -0700

    latest

--- mobile-app/www/remote/js/controllers/util/BugsCtrl.js
+++ mobile-app/www/remote/js/controllers/util/BugsCtrl.js
@@ -10,26 +10,30 @@ angular.module('uguru.util.controllers')
   'LoadingService',
   function($scope, $state, $timeout, FileService, LoadingService) {
     $scope.bugs = [];
+    $scope.help = {};
 
-
-
-    $scope.$on('$ionicView.enter', function() {
+    $scope.$on('$ionicView.beforeEnter', function() {
 
       loadUpdatedBugsJsonFile($scope);
-
     })
 
     function loadUpdatedBugsJsonFile(scope) {
       LoadingService.showAmbig('Loading....', 10000);
+      //https://s3.amazonaws.com/uguru-admin/jason/bugs.json
+      //https://s3.amazonaws.com/uguru-admin/sync/bugs.json
       FileService.getS3JsonFile(null, 'https://s3.amazonaws.com/uguru-admin/sync/bugs.json', callbackFunc);
       function callbackFunc(name, resp) {
-        scope.bugs = resp;
+        scope.help = resp.help
+        scope.bugs = resp.bugs;
         LoadingService.hide()
         $timeout(function() {
          LoadingService.showSuccess(resp.length + ' bugs loaded', 1000) ;
         })
       }
     }
+    setTimeout(function() {
+      console.log($scope.bugs)
+    }, 1000);
 
 
 
