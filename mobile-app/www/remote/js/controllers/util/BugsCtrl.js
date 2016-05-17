30d3c41756a3df21dea6a54144a1da6a5a900ebf
Jason Huang <f230536@hotmail.com>
2016-05-16 17:59:31 -0700

    latest

--- mobile-app/www/remote/js/controllers/util/BugsCtrl.js
+++ mobile-app/www/remote/js/controllers/util/BugsCtrl.js
@@ -11,9 +11,18 @@ angular.module('uguru.util.controllers')
   function($scope, $state, $timeout, FileService, LoadingService) {
     $scope.bugs = [];
     $scope.help = {};
+    $scope.listOption = ['All Bugs', 'Prioritized Bugs','Recently Complete'];
+    $scope.selectOption = $scope.listOption[0]
+    $scope.openBugList=function(section){
+        console.log("Click",section.name)
+        $scope.bugs = section.bugs
+        $scope.help = section.help
+        $scope.name = section.name
+    }
+
 
     $scope.$on('$ionicView.beforeEnter', function() {
-
+  
       loadUpdatedBugsJsonFile($scope);
     })
 
@@ -23,8 +32,7 @@ angular.module('uguru.util.controllers')
       //https://s3.amazonaws.com/uguru-admin/sync/bugs.json
       FileService.getS3JsonFile(null, 'https://s3.amazonaws.com/uguru-admin/sync/bugs.json', callbackFunc);
       function callbackFunc(name, resp) {
-        scope.help = resp.help
-        scope.bugs = resp.bugs;
+        scope.bugReport = resp
         LoadingService.hide()
         $timeout(function() {
          LoadingService.showSuccess(resp.length + ' bugs loaded', 1000) ;
@@ -32,7 +40,7 @@ angular.module('uguru.util.controllers')
       }
     }
     setTimeout(function() {
-      console.log($scope.bugs)
+      console.log($scope.bugReport)
     }, 1000);
 
 
