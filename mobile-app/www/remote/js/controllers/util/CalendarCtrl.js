d1ef197cb62efa01536f8c2c8a1180231c19a251
Samir Makhani <sam1rm@berkeley.edu>
2016-05-18 14:49:15 -0700

    {most updated}

--- mobile-app/www/remote/js/controllers/util/CalendarCtrl.js
+++ mobile-app/www/remote/js/controllers/util/CalendarCtrl.js
@@ -10,7 +10,8 @@ angular.module('uguru.util.controllers')
   'KeyboardService',
   '$window',
   function($scope, $state, $timeout, $localstorage, $ionicSlideBoxDelegate, CalendarService, KeyboardService, $window) {
-    var gDocUrl = 'https://docs.google.com/document/d/1Xsnsv2cZTvRtUPv9NWXJDG5mMyYYi_RmDZkGsve6xLU/edit'
+    var gDocUrl = 'https://docs.google.com/document/d/1Xsnsv2cZTvRtUPv9NWXJDG5mMyYYi_RmDZkGsve6xLU/edit';
+    var codepenSpecUrl = 'http://codepen.io/teamuguru/pen/ddd2f97039f2fec817d52499dd3c00ac.js';
     var cpFiles = [
       'https://uguru-rest-test.herokuapp.com/static/remote/js/min.app.js'
     ];
@@ -22,9 +23,12 @@ angular.module('uguru.util.controllers')
         toggleSpec: false
     }
 
+    $scope.spec = {};
+
     // $scope.$on('$ionicView.loaded', function() {
       $timeout(function() {
         $scope.calendar = CalendarService.getNextSevenDaysArr()
+        loadCodepenSpec();
         KeyboardService.initOptionPressedAndReleasedFunction(toggleDev, null, 68, 'd', true, null);
         KeyboardService.initOptionPressedAndReleasedFunction(toggleSpec, null, 83, 's', true, null);
         KeyboardService.initOptionPressedAndReleasedFunction(toggleSpec, null, 27, 'esc', true, null);
@@ -33,6 +37,21 @@ angular.module('uguru.util.controllers')
         }, 5000)
       }, 1000)
     // })
+
+    function loadCodepenSpec() {
+      var xhr = new XMLHttpRequest();
+      xhr.open( 'GET', codepenSpecUrl, true );
+
+      xhr.onload = function () {
+          $scope.spec = JSON.parse(xhr.responseText);
+
+          $timeout(function() {
+            $scope.$apply();
+          });
+      };
+      xhr.send();
+
+    }
 
     function toggleDev() {
       $scope.dev.toggleDev = !$scope.dev.toggleDev;
@@ -45,6 +64,10 @@ angular.module('uguru.util.controllers')
     $scope.openDocInNewTab = function(){
       $window.open(gDocUrl, '_blank');
     };
+
+    $scope.openCodepenSpec = function() {
+      $window.open(codepenSpecUrl.replace('.js','') + '/?editors=0010', '_blank');
+    }
 
     function getHtmlString() {
       var calendar_view_html = document.querySelector('#calendar-view').parentNode.innerHTML;
