848f00a8ce5a1f5f12782872bd207a0b6197c5c9
Samir Makhani <sam1rm@berkeley.edu>
2016-05-17 22:23:52 -0700

    most updated

--- app/static/remote/js/controllers/util/CalendarCtrl.js
+++ app/static/remote/js/controllers/util/CalendarCtrl.js
@@ -12,18 +12,24 @@ angular.module('uguru.util.controllers')
   function($scope, $state, $timeout, $localstorage, $ionicSlideBoxDelegate, CalendarService, KeyboardService, $window) {
     var gDocUrl = 'https://docs.google.com/document/d/1Xsnsv2cZTvRtUPv9NWXJDG5mMyYYi_RmDZkGsve6xLU/edit'
     var cpFiles = [
-      'https://uguru-rest-test.herokuapp.com/static/remote/lib/ionic/js/ionic.bundle.min.v2.js',
-      'https://uguru-rest-test.herokuapp.com/static/remote/lib/restangular/dist/restangular.js',
       'https://uguru-rest-test.herokuapp.com/static/remote/js/min.app.js'
-
     ];
 
+    $scope.dev = {
+        viewsDropdown: {},
+        screenSizesDropdown: {label: "screen sizes", size: "small", options: ["desktop", "mobile"], selectedIndex:0, onOptionClick: switchScreenSize},
+        toggleDev: false,
+        toggleSpec: false
+    }
+
     // $scope.$on('$ionicView.loaded', function() {
-      $scope.calendar = CalendarService.getNextSevenDaysArr()
-      KeyboardService.initOptionPressedAndReleasedFunction(toggleDev, null, 68, 'd', true, null);
-      KeyboardService.initOptionPressedAndReleasedFunction(toggleSpec, null, 83, 's', true, null);
-      KeyboardService.initOptionPressedAndReleasedFunction(toggleSpec, null, 27, 'esc', true, null);
-      initDevOptions();
+      $timeout(function() {
+        $scope.calendar = CalendarService.getNextSevenDaysArr()
+        KeyboardService.initOptionPressedAndReleasedFunction(toggleDev, null, 68, 'd', true, null);
+        KeyboardService.initOptionPressedAndReleasedFunction(toggleSpec, null, 83, 's', true, null);
+        KeyboardService.initOptionPressedAndReleasedFunction(toggleSpec, null, 27, 'esc', true, null);
+        generateCodePenData();
+      }, 1000)
     // })
 
     function toggleDev() {
@@ -39,31 +45,39 @@ angular.module('uguru.util.controllers')
     };
 
     function getHtmlString() {
-      return '<ui-view id="uguru-view" ng-app="uguru"> <script type="text/ng-template" id="view1.html"> <div ng-include="' + "'remote/templates/calendar.html'" + '"> VIEW 1 </div> </script> <ui-view>'
+      var calendar_view_html = document.querySelector('#calendar-view').parentNode.innerHTML;
+      return '<div id="fb-root"></div><body ng-app="uguru" animation="slide-left-right-ios7"><ui-view id="uguru-view"><script type="text/ng-template" id="view1.html">' + calendar_view_html + '</div></script></ui-view></body>'
+    }
+    function generateCodePenData () {
+
+      $scope.codepenData = {
+        title                 : "Uguru Calendar",
+        description           : "Most updated version",
+        private               : true, // true || false
+        tags                  : [], // an array of strings
+        editors               : "101", // Set which editors are open. In this example HTML open, CSS closed, JS open
+        layout                : "right", // top | left | right
+        html                  : getHtmlString(),
+        html_pre_processor    : "",
+        css                   : "html { color: red; }",
+        css_pre_processor     : "scss",
+        css_starter           : "neither",
+        css_prefix            : "autoprefixer",
+        js                    : "_local=true;",
+        js_pre_processor      : "none",
+        html_classes          : null,
+        head                  : "<meta name='viewport' content='width=device-width'><script>_local=false;_startpage='calendar';</script>",
+        css_external          : "https://uguru-rest-test.herokuapp.com/static/remote/css/app_version.css", // semi-colon separate multiple files
+        js_external           : 'https://uguru-rest-test.herokuapp.com/static/remote/js/min.app.js', // semi-colon separate multiple files
+        css_pre_processor_lib : null,
+        js_modernizr : null,
+        js_library   : null,
+      }
+
     }
 
-    $scope.codepenData = {
-      title                 : "Uguru Calendar",
-      description           : "Most updated version",
-      private               : true, // true || false
-      tags                  : [], // an array of strings
-      editors               : "101", // Set which editors are open. In this example HTML open, CSS closed, JS open
-      layout                : "right", // top | left | right
-      html                  : getHtmlString(),
-      html_pre_processor    : "",
-      css                   : "html { color: red; }",
-      css_pre_processor     : "scss",
-      css_starter           : "neither",
-      css_prefix            : "autoprefixer",
-      js                    : "_local=true;",
-      js_pre_processor      : "none",
-      html_classes          : null,
-      head                  : "<meta name='viewport' content='width=device-width'><script>_local=true;_startpage='calendar';</script>",
-      css_external          : "https://uguru-rest-test.herokuapp.com/static/remote/css/app_version.css", // semi-colon separate multiple files
-      js_external           : cpFiles.join(';'), // semi-colon separate multiple files
-      css_pre_processor_lib : null,
-      js_modernizr : null,
-      js_library   : null,
+    function switchScreenSize() {
+      return;
     }
 
     // toggleDev();
@@ -72,18 +86,6 @@ angular.module('uguru.util.controllers')
     //   document.querySelector('#codepen-input').click();
     // }, 1500);
 
-
-    function initDevOptions() {
-      $scope.dev = {
-        viewsDropdown: {},
-        screenSizesDropdown: {label: "screen sizes", size: "small", options: ["desktop", "mobile"], selectedIndex:0, onOptionClick: switchScreenSize},
-      }
-
-
-      function switchScreenSize() {
-
-      }
-    }
   }
 
 ])
