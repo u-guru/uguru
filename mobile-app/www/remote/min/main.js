bc81846e2e6f5ec3efeb6dfe480c68c475f228cf
Gabrielle Wee <mail@gabriellew.ee>
2016-05-28 12:00:26 -0700

    update

--- mobile-app/www/remote/min/main.js
+++ mobile-app/www/remote/min/main.js
@@ -58,6 +58,16 @@ angular.module('uguru', ['ionic', 'restangular', 'ngAnimate', 'uguru.preApp',
     controller: 'AdminController',
     templateUrl: 'admin/templates/index.html',
   })
+  .state('root.jeselle', {
+    url:'/jeselle',
+    controller: 'JeselleController',
+    templateUrl: 'jeselle/templates/index.html',
+  })
+  .state('root.gabrie', {
+    url:'/gabrie',
+    controller: 'GabrielleController',
+    templateUrl: 'gabrielle/templates/index.html',
+  })
 
   $urlRouterProvider.otherwise('/');
 
