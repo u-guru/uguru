8e5d490c16e2b83820c7e64bb0488875b3f1456e
Samir Makhani <sam1rm@berkeley.edu>
2016-07-02 17:33:03 -0700

    most updated

--- app/static/remote/min/admin/js/SpecService.js
+++ app/static/remote/min/admin/js/SpecService.js
@@ -32,7 +32,7 @@ function SpecService($state, $timeout, $localstorage, $window, $compile, $sce, K
         if (window.location.href.split('codepen').length > 1) {
             extraDelay = 1500;
         }
-        if (!isParamLastPartUrl(param) || (window.location.href.split('/dev/').length === 1) && window.location.href.split('codepen').length === 1) {
+        if (!isParamLastPartUrl(param) && (window.location.href.split('/dev/').length === 1 || (window.location.href.split('codepen').length === 1))) {
             return;
         }
         $timeout(function() {
