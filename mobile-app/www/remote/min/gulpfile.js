4d781c2598db911120158fb72a7edc84fad1fc34
Samir Makhani <sam1rm@berkeley.edu>
2016-08-01 17:40:45 -0700

    {most udpated}

--- mobile-app/www/remote/min/gulpfile.js
+++ mobile-app/www/remote/min/gulpfile.js
@@ -359,7 +359,7 @@ gulp.task('copy-prod', function(){
 
 gulp.task('default', function(done) {
   runSequence(
-    'clean',
+    // 'clean',
     'compile-css',
     'templates',
     'compile-temp',
