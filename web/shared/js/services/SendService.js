534a3aeaa1a767bf37f0191f7f0bd70ae2cc8e24
Samir Makhani <sam1rm@berkeley.edu>
2016-10-25 17:33:54 -0700

    most updated

--- web/shared/js/services/SendService.js
+++ web/shared/js/services/SendService.js
@@ -28,7 +28,7 @@ function SendService($timeout) {
   function prepareToSendMessage(msg_name, msg_str, scope) {
     scope.public.customStates.when[msg_name] = {elements: [], depth: 0};
     var msg_scope = msg_str.split(':')[1];
-
+    console.log(msg_scope, msg_name)
     if (msg_scope === 'siblings' || msg_scope ==='depth(-0)' || msg_scope === 'depth(0)') {
       if (!scope.$parent.public) {
         scope.$parent.public = {customStates: {when: {}}};
@@ -38,37 +38,29 @@ function SendService($timeout) {
       $timeout(function() {
         scope.$apply();
       })
-    } else if (msg_scope === 'depth') {
-      scope.public.customStates.when[msg_name] = msg_scope.split('depth(')[0].replace(')', '').replace('<', '').replace('>', '');
-      scope.root.scope.public.customStates.when[msg_name] = {elements: [], depth: 0};
     }
 
   }
 
-  function sendMsgToSelf(element, scope, state_ref, msg_type, full_msg_name) {
+  function sendMsgToSelf(element, scope, state_ref, msg_type) {
     msg_type = msg_type.trim();
     state_ref = state_ref.trim();
-    if (['with', 'init', 'as'].indexOf(state_ref) > -1) {
+    if (['with', 'init', 'as'].indexOf(state_ref) > -1 || msg_type === 'when') {
 
       // if (state_ref && state_ref.length && typeof) {``
 
       //   internal_name = state_ref[0];
       // }
       var internal_name = state_ref;
+      console.log(scope.states)
       if (scope.states[msg_type] && scope.states[msg_type].length) {
         scope.states[msg_type].forEach(function(state, i) {
 
-          if (state.name.indexOf(state_ref) > -1) {
+          if (state.name.indexOf(state_ref) > -1 || msg_type === 'when') {
             state.exec(element, scope, null);
           }
           // if (on_state.name && on_state.name.indexOf())
         })
-      }
-    }
-    if (msg_type === 'when') {
-      if (scope.public.customStates.when) {
-        console.log(scope.states, scope.public.customStates.when, full_msg_name);
-
       }
     }
   }
